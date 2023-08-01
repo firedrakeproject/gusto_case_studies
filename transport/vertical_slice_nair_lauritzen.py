405fe05580d7ae5051a586ea97daec511c38e55d
Tim Andrews <ta440@github.com>
2023-08-01 16:20:05 +0100

    verified the two setups are working

--- transport/vertical_slice_nair_lauritzen.py
+++ transport/vertical_slice_nair_lauritzen.py
@@ -18,65 +18,64 @@ The mixing ratio obeys an advective transport equation:
 Whereas the dry density obeys the conservative form:
 ∂/∂t (ρ_d) + ∇.(ρ_d*u) = 0
 
-The relation between these is given by 
-
-m_X = ρ_X/ρ_d
+There are two configurations that can be run:
+  The 'convergence' configuration has an initial condition of a linearly 
+  varying density field and two Gaussian bumps for the mixing ratio.
+  The 'consistency' configuration has an initial condition of a 
+  constant mixing ratio and two Gaussian bumps for the density.
 
 """
 
-
 # Specify whether to run the 'convergence' or 'consistency' version of the test.
-case = 'convergence'
+case = 'consistency'
 
 # Choose space for the mixing ratio.
-# Theta if want staggered, DG if want colocated
+# Theta will make it staggered relative to the density
+# DG will make it colocated with the density
 m_X_space = 'DG'
 
 # Domain
-Lx = 2000
-Hz = 2000
-
-dx = 160
-dz = 160
+Lx = 2000.
+Hz = 2000.
 
 # Time parameters
 dt = 2.
 tmax = 2000.
 
+nlayers = 200.  # horizontal layers
+columns = 200.  # number of columns
 
-
-nlayers = int(Hz/dz)  # horizontal layers
-columns = int(Lx/dx)  # number of columns
+dx = Lx/nlayers
+dz = Hz/columns
 
 period_mesh = PeriodicIntervalMesh(columns, Lx)
 mesh = ExtrudedMesh(period_mesh, layers=nlayers, layer_height=Hz/nlayers)
 domain = Domain(mesh, dt, "CG", 1)
 x,z = SpatialCoordinate(mesh)
 
+# Define the mixing ratio and density as tracers
 m_X = ActiveTracer(name='m_X', space=m_X_space,
                  variable_type=TracerVariableType.mixing_ratio,
-                 transport_eqn=TransportEquationType.conservative)
+                 transport_eqn=TransportEquationType.advective)
                  
 rho_d = ActiveTracer(name='rho_d', space='DG',
                  variable_type=TracerVariableType.density,
                  transport_eqn=TransportEquationType.conservative)
 
-# Define the tracers of mass and dry density
 tracers = [m_X,rho_d]
 
 # Equation
-V = domain.spaces("DG")
+V = domain.spaces("HDiv")
 eqn = CoupledTransportEquation(domain, active_tracers=tracers, Vu = V)
 
 # I/O
-dirname = "vertical_slice_nair_lauritzen"
+dirname = "vertical_slice_nair_lauritzen_"+case
 
 # Dump the solution at each day
 dumpfreq = int(100./dt)
 
 # Set dump_nc = True to use tomplot.
 output = OutputParameters(dirname=dirname,
-                          dumplist_latlon=['D'],
                           dumpfreq = dumpfreq,
                           log_level="INFO",
                           dump_nc = True,
@@ -89,7 +88,7 @@ U = Lx/tmax
 W = U/10.
 
 def u_t(t):
-  xd = x - (Lx/2) - U*t
+  xd = x - U*t
   u = U - (W*pi*Lx/Hz)*cos(pi*t/tmax)*cos(2*pi*xd/Lx)*cos(pi*z/Hz)
   w = 2*pi*W*cos(pi*t/tmax)*sin(2*pi*xd/Lx)*sin(pi*z/Hz)
   
@@ -98,61 +97,58 @@ def u_t(t):
   return u_expr
 
 # Specify locations of the two Gaussians
-xc1 = Lx/8.
+xc1 = 5.*Lx/8.
 zc1 = Hz/2.
 
-xc2 = -Lx/8.
+xc2 = 3.*Lx/8.
 zc2 = Hz/2.
 
 def l2_dist(xc,zc):
   return min_value(abs(x-xc), Lx-abs(x-xc))**2 + (z-zc)**2
 
+lc = 2.*Lx/25.
+m0 = 0.02
+
+# Set the initial states from the choice of configuration
 if case == 'convergence':
   f0 = 0.05
   
   rho_t = 0.5
   rho_b = 1.
   
-  rho_d_0 = rho_b + z*(rho_b-rho_t)/Hz
+  rho_d_0 = rho_b + z*(rho_t-rho_b)/Hz  
   
-  m0 = 0.02
-  
-  lc = 2.*Lx/25.
-  
-  g1 = f0*exp(l2_dist(xc1,zc1)/(lc**2))
-  g2 = f0*exp(l2_dist(xc2,zc2)/(lc**2))
+  g1 = f0*exp(-l2_dist(xc1,zc1)/(lc**2))
+  g2 = f0*exp(-l2_dist(xc2,zc2)/(lc**2))
   
   m_X_0 = m0 + g1 + g2
   
 elif case == 'consistency':
   f0 = 0.5
+  rho_b = 0.5
   
-  m0 = 0.02
-  rho_b = 0.05
+  g1 = f0*exp(-l2_dist(xc1,zc1)/(lc**2))
+  g2 = f0*exp(-l2_dist(xc2,zc2)/(lc**2))
   
-  g1 = f0*exp(l2_dist(xc1,zc1)/(lc**2))
-  g2 = f0*exp(l2_dist(xc2,zc2)/(lc**2))
+  rho_d_0 = rho_b + g1 + g2
   
-  rho_d = rho_b + g1 + g2
-  
-  m_X_0 = m0
+  m_X_0 = m0 + 0*x
   
 else:
   raise NotImplementedError('Specified case is not recognised.')
 
 
 transport_scheme = SSPRK3(domain)
-#transport_methods = [DGUpwind(eqn, "m_X"), DGUpwind(eqn, "rho_d")]
-transport_methods = []
+transport_methods = [DGUpwind(eqn, "m_X"), DGUpwind(eqn, "rho_d")]
     
 # Time stepper
 stepper = PrescribedTransport(eqn, transport_scheme, io, transport_methods,
                               prescribed_transporting_velocity=u_t)
 
 # initial conditions
-m_X_0 = stepper.fields("m_X")
-rho_d_0 = stepper.fields("rho_d")
-D0.interpolate(Dexpr)
+stepper.fields("m_X").interpolate(m_X_0)
+stepper.fields("rho_d").interpolate(rho_d_0)
+u0 = stepper.fields("u")
 u0.project(u_t(0))
 
 stepper.run(t=0, tmax=tmax)
