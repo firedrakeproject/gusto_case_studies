e7732d37b1630b05de3aa7162dc6c9981505a663
dw603@exeter.ac.uk <dw603@exeter.ac.uk>
2023-10-11 09:56:22 +0100

    dt still too high for courant number

--- compressible/baroclinic_wave.py
+++ compressible/baroclinic_wave.py
@@ -44,7 +44,7 @@ from gusto import *                                            #
 # Configuratio Options
 # -------------------------------------------------------------- #
 config = 'config4'
-dt = 450.
+dt = 225.
 days = 15.
 tmax = days * 24. * 60. * 60.
 n = 24   # cells per cubed sphere face edge
@@ -203,7 +203,7 @@ eqn = CompressibleEulerEquations(domain, params, Omega=Omega, u_transport_option
 print(f'Number of DOFs = {eqn.X.function_space().dim()}')
 
 output = OutputParameters(dirname=dirname,
-                          dumpfreq=24, # every 3 hours
+                          dumpfreq=48, # every 3 hours
                           dump_nc=True,
                           dump_vtus=False)
 diagnostic_fields = [MeridionalComponent('u'), ZonalComponent('u'),RadialComponent('u'),
