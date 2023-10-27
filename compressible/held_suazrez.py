f2696e7472f7e6491050efe7677be995f0393132
dw603@exeter.ac.uk <dw603@exeter.ac.uk>
2023-10-27 10:19:23 +0100

    200day run

--- compressible/held_suazrez.py
+++ compressible/held_suazrez.py
@@ -90,7 +90,7 @@ eqn = CompressibleEulerEquations(domain, params, Omega=Omega, u_transport_option
 print(f'Number of DOFs = {eqn.X.function_space().dim()}')
 
 
-dirname = 'Held_suarez_temp_and_vel'
+dirname = f'Held_suarez_temp_and_vel_tmax={days}days'
 output = OutputParameters(dirname=dirname,
                           dumpfreq=9, # every 3 hours
                           dump_nc=True,
