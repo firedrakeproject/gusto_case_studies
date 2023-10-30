9425123a732e6a4cd8a7c320181477d02ff8fd53
dw603@exeter.ac.uk <dw603@exeter.ac.uk>
2023-10-30 12:26:48 +0000

    changed dirname

--- compressible/held_suazrez.py
+++ compressible/held_suazrez.py
@@ -90,7 +90,7 @@ eqn = CompressibleEulerEquations(domain, params, Omega=Omega, u_transport_option
 print(f'Number of DOFs = {eqn.X.function_space().dim()}')
 
 
-dirname = f'Held_suarez_test'
+dirname = f'HS_temp_vel'
 output = OutputParameters(dirname=dirname,
                           dumpfreq=9, # every 3 hours
                           dump_nc=True,
