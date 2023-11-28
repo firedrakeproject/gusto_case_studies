1d6e488378efe2deaea1d9868d909307472e8b46
Witt-D <dw603@exeter.ac.uk>
2023-11-28 11:11:12 +0000

    looping through degree options

--- compressible/rising_bubble_degreetest.py
+++ compressible/rising_bubble_degreetest.py
@@ -13,11 +13,11 @@ from icecream import ic
 # Test case parameters
 # ---------------------------------------------------------------------------- #
 
-dt = 1.
+dt = 0.1
 L = 1000.
 H = 1000.
 tmax = 600.
-dumpfreq = int(tmax / (6*dt))
+dumpfreq = int(tmax / (60*dt))
 res = 5.
 nlayers = int(H / res)
 ncolumns = int(L  /res)
@@ -25,119 +25,124 @@ ncolumns = int(L  /res)
 # ---------------------------------------------------------------------------- #
 # Set up model objects
 # ---------------------------------------------------------------------------- #
+degrees = [(0,0), (0,1), (1,0), (1,1)]
+for degree in degrees:
+	# Domain
+	h_degree = degree[0]
+	v_degree = degree[1]
+	if v_degree == 0:
+	    nlayers = nlayers * 2
+	if h_degree ==0:
+	    ncoloumns = ncolumns * 2
+	m = PeriodicIntervalMesh(ncolumns, L)
+	mesh = ExtrudedMesh(m, layers=nlayers, layer_height=H/nlayers)
+	domain = Domain(mesh, dt, "CG", 
+			horizontal_degree=h_degree, 
+			vertical_degree=v_degree)
 
-# Domain
-v_degree = 0
-h_degree = 1
-m = PeriodicIntervalMesh(ncolumns, L)
-mesh = ExtrudedMesh(m, layers=nlayers, layer_height=H/nlayers)
-domain = Domain(mesh, dt, "CG", 
-                horizontal_degree=h_degree, 
-                vertical_degree=v_degree)
+	# Equation
+	parameters = CompressibleParameters()
+	ic('making equation')
+	eqn = CompressibleEulerEquations(domain, parameters)
+	print(f'ideal number of cores = {eqn.X.function_space().dim() / 50000}')
+	# I/O
+	dirname = f'RB_horiz={h_degree}_vertical={v_degree}_res={res}'
+	output = OutputParameters(dirname=dirname,
+				  dumpfreq=dumpfreq,
+				  dumplist=['u'],
+				  dump_nc = True,
+				  dump_vtus = False)
+	diagnostic_fields = [CourantNumber(), Perturbation('theta'), Perturbation('rho')]
+	io = IO(domain, output, diagnostic_fields=diagnostic_fields)
 
-# Equation
-parameters = CompressibleParameters()
-ic('making equation')
-eqn = CompressibleEulerEquations(domain, parameters)
-print(f'ideal number of cores = {eqn.X.function_space().dim() / 50000}')
-# I/O
-dirname = f'RB_horiz={h_degree}_vertical={v_degree}_res={res}'
-output = OutputParameters(dirname=dirname,
-                          dumpfreq=dumpfreq,
-                          dumplist=['u'],
-                          dump_nc = True,
-                          dump_vtus = False)
-diagnostic_fields = [CourantNumber(), Perturbation('theta'), Perturbation('rho')]
-io = IO(domain, output, diagnostic_fields=diagnostic_fields)
-
-VDG1 = domain.spaces("DG1_equispaced")
-VCG1 = FunctionSpace(mesh, "CG", 1)
-Vu_DG1 = VectorFunctionSpace(mesh, VDG1.ufl_element())
-Vu_CG1 = VectorFunctionSpace(mesh, "CG", 1)
-
-
-u_opts = RecoveryOptions(embedding_space=Vu_DG1,
-                        recovered_space=Vu_CG1)
-
-rho_opts = RecoveryOptions(embedding_space=VDG1,
-                        recovered_space=VCG1)
-
-if v_degree == 0:
-    u_opts = RecoveryOptions(embedding_space=Vu_DG1,
-                        recovered_space=Vu_CG1,
-                        boundary_method=BoundaryMethod.taylor)
-    rho_opts = RecoveryOptions(embedding_space=VDG1,
-                        recovered_space=VCG1,
-                        boundary_method=BoundaryMethod.taylor)
+	VDG1 = domain.spaces("DG1_equispaced")
+	VCG1 = FunctionSpace(mesh, "CG", 1)
+	Vu_DG1 = VectorFunctionSpace(mesh, VDG1.ufl_element())
+	Vu_CG1 = VectorFunctionSpace(mesh, "CG", 1)
 
 
-theta_opts = RecoveryOptions(embedding_space=VDG1,
-                            recovered_space=VCG1)
+	u_opts = RecoveryOptions(embedding_space=Vu_DG1,
+				recovered_space=Vu_CG1)
 
-transported_fields = []
-transported_fields.append(SSPRK3(domain, "u", options=u_opts))
-transported_fields.append(SSPRK3(domain, "rho", options=rho_opts))
-transported_fields.append(SSPRK3(domain, "theta", options=theta_opts))
+	rho_opts = RecoveryOptions(embedding_space=VDG1,
+				recovered_space=VCG1)
 
-transport_methods = [DGUpwind(eqn, 'u'),
-                    DGUpwind(eqn, 'rho'),
-                    DGUpwind(eqn, 'theta')]
+	if v_degree == 0:
+	    u_opts = RecoveryOptions(embedding_space=Vu_DG1,
+				recovered_space=Vu_CG1,
+				boundary_method=BoundaryMethod.taylor)
+	    rho_opts = RecoveryOptions(embedding_space=VDG1,
+				recovered_space=VCG1,
+				boundary_method=BoundaryMethod.taylor)
 
-# Transport schemes
-theta_opts = EmbeddedDGOptions()
-transported_fields = [TrapeziumRule(domain, "u"),
-                      SSPRK3(domain, "rho"),
-                      SSPRK3(domain, "theta", options=theta_opts)]
 
-transport_methods = [DGUpwind(eqn, "u"),
-                     DGUpwind(eqn, "rho"),
-                     DGUpwind(eqn, "theta")]
+	theta_opts = RecoveryOptions(embedding_space=VDG1,
+				    recovered_space=VCG1)
 
-# Linear solver
-linear_solver = CompressibleSolver(eqn)
+	transported_fields = []
+	transported_fields.append(SSPRK3(domain, "u", options=u_opts))
+	transported_fields.append(SSPRK3(domain, "rho", options=rho_opts))
+	transported_fields.append(SSPRK3(domain, "theta", options=theta_opts))
 
-# Time stepper
-stepper = SemiImplicitQuasiNewton(eqn, io, transported_fields,
-                                  transport_methods,
-                                  linear_solver=linear_solver)
+	transport_methods = [DGUpwind(eqn, 'u'),
+			    DGUpwind(eqn, 'rho'),
+			    DGUpwind(eqn, 'theta')]
 
-# ---------------------------------------------------------------------------- #
-# Initial conditions
-# ---------------------------------------------------------------------------- #
+	# Transport schemes
+	theta_opts = EmbeddedDGOptions()
+	transported_fields = [TrapeziumRule(domain, "u"),
+			      SSPRK3(domain, "rho"),
+			      SSPRK3(domain, "theta", options=theta_opts)]
 
-u0 = stepper.fields("u")
-rho0 = stepper.fields("rho")
-theta0 = stepper.fields("theta")
+	transport_methods = [DGUpwind(eqn, "u"),
+			     DGUpwind(eqn, "rho"),
+			     DGUpwind(eqn, "theta")]
 
-# spaces
-Vu = domain.spaces("HDiv")
-Vt = domain.spaces("theta")
-Vr = domain.spaces("DG")
+	# Linear solver
+	linear_solver = CompressibleSolver(eqn)
 
-# Isentropic background state
-Tsurf = Constant(300.)
+	# Time stepper
+	stepper = SemiImplicitQuasiNewton(eqn, io, transported_fields,
+					  transport_methods,
+					  linear_solver=linear_solver)
 
-theta_b = Function(Vt).interpolate(Tsurf)
-rho_b = Function(Vr)
+	# ---------------------------------------------------------------------------- #
+	# Initial conditions
+	# ---------------------------------------------------------------------------- #
 
-# Calculate hydrostatic exner
-compressible_hydrostatic_balance(eqn, theta_b, rho_b, solve_for_rho=True)
+	u0 = stepper.fields("u")
+	rho0 = stepper.fields("rho")
+	theta0 = stepper.fields("theta")
 
-x = SpatialCoordinate(mesh)
-xc = 500.
-zc = 350.
-rc = 250.
-r = sqrt((x[0]-xc)**2 + (x[1]-zc)**2)
-theta_pert = conditional(r > rc, 0., 0.25*(1. + cos((pi/rc)*r)))
+	# spaces
+	Vu = domain.spaces("HDiv")
+	Vt = domain.spaces("theta")
+	Vr = domain.spaces("DG")
 
-theta0.interpolate(theta_b + theta_pert)
-rho0.interpolate(rho_b)
+	# Isentropic background state
+	Tsurf = Constant(300.)
 
-stepper.set_reference_profiles([('rho', rho_b),
-                                ('theta', theta_b)])
+	theta_b = Function(Vt).interpolate(Tsurf)
+	rho_b = Function(Vr)
 
-# ---------------------------------------------------------------------------- #
-# Run
-# ---------------------------------------------------------------------------- #
+	# Calculate hydrostatic exner
+	compressible_hydrostatic_balance(eqn, theta_b, rho_b, solve_for_rho=True)
 
-stepper.run(t=0, tmax=tmax)
+	x = SpatialCoordinate(mesh)
+	xc = 500.
+	zc = 350.
+	rc = 250.
+	r = sqrt((x[0]-xc)**2 + (x[1]-zc)**2)
+	theta_pert = conditional(r > rc, 0., 0.25*(1. + cos((pi/rc)*r)))
+
+	theta0.interpolate(theta_b + theta_pert)
+	rho0.interpolate(rho_b)
+
+	stepper.set_reference_profiles([('rho', rho_b),
+					('theta', theta_b)])
+
+	# ---------------------------------------------------------------------------- #
+	# Run
+	# ---------------------------------------------------------------------------- #
+
+	stepper.run(t=0, tmax=tmax)
