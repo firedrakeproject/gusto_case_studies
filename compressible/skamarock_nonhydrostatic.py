8716f8d4a42bb3e9f190818a0bb4c2d45bd873b4
Witt-D <dw603@exeter.ac.uk>
2024-01-10 14:43:49 +0000

    changed how the recvoery spaces are made

--- compressible/skamarock_nonhydrostatic.py
+++ compressible/skamarock_nonhydrostatic.py
@@ -3,35 +3,48 @@ The non-linear gravity wave test case of Skamarock and Klemp (1994).
 
 Potential temperature is transported using SUPG.
 """
-
 from petsc4py import PETSc
-from pyrsistent import v
-PETSc.Sys.popErrorHandler()
 from gusto import *
 import itertools
-from firedrake import (as_vector, SpatialCoordinate, PeriodicIntervalMesh,
-                       ExtrudedMesh, exp, sin, Function, pi, COMM_WORLD)
+from firedrake import (as_vector, SpatialCoordinate, PeriodicIntervalMesh, TensorProductElement,
+                       ExtrudedMesh, exp, sin, Function, pi, FiniteElement, cell, interval)
 import numpy as np
 import icecream as ic
-import sys
 
-def lowestorderspaces(domain, BC=None):
-    # creates lowest order spaces
-    mesh=domain.mesh
-    VDG1 = domain.spaces("DG1_equispaced")
-    VCG1 = FunctionSpace(mesh, "CG", 1)
-    Vu_DG1 = VectorFunctionSpace(mesh, VDG1.ufl_element())
-    Vu_CG1 = VectorFunctionSpace(mesh, "CG", 1)
 
-    u_opts = RecoveryOptions(embedding_space=Vu_DG1,
-				             recovered_space=Vu_CG1,
+def RecoverySpaces(mesh, vertical_degree, horizontal_degree, BC=None):
+    # scaler spaces
+    DG_vert_ele = FiniteElement('DG', interval, vertical_degree+1, variant='equispaced')
+    DG_hori_ele = FiniteElement('DG', cell, horizontal_degree+1, variant='equispaced')
+    CG_hori_ele = FiniteElement('CG', cell, vertical_degree+1)
+    CG_vert_ele = FiniteElement('CG', interval, vertical_degree+1)
+
+    VDG_ele = TensorProductElement(DG_hori_ele, DG_vert_ele)
+    VCG_ele = TensorProductElement(CG_hori_ele, CG_vert_ele)
+    VDG = FunctionSpace(mesh, VDG_ele)
+    VCG = FunctionSpace(mesh, VCG_ele)
+
+    # vector_spaces
+    DG_hori_vec_ele = FiniteElement('DG', cell, horizontal_degree+2)
+    DG_vert_vec_ele = FiniteElement('DG', interval, horizontal_degree+2)
+    CG_hori_vec_ele = FiniteElement('hdiv', cell, horizontal_degree+2)
+    CG_vert_vec_ele = FiniteElement('hdiv', interval, horizontal_degree+2)
+
+    VDG_Vec_ele = TensorProductElement(DG_hori_vec_ele, DG_vert_vec_ele)
+    VCG_Vec_ele = TensorProductElement(CG_hori_vec_ele, CG_vert_vec_ele)
+
+    Vu_DG = VectorFunctionSpace(mesh, VDG_Vec_ele)
+    Vu_CG = VectorFunctionSpace(mesh, VCG_Vec_ele)
+
+    u_opts = RecoveryOptions(embedding_space=Vu_DG,
+                             recovered_space=Vu_CG,
                              boundary_method=BC)
-    rho_opts = RecoveryOptions(embedding_space=VDG1,
-				               recovered_space=VCG1,
+    rho_opts = RecoveryOptions(embedding_space=VDG,
+                               recovered_space=VCG,
                                boundary_method=BC)
-    theta_opts = RecoveryOptions(embedding_space=VDG1,
-				                 recovered_space=VCG1)
-    
+    theta_opts = RecoveryOptions(embedding_space=VDG,
+                                 recovered_space=VCG)
+
     return u_opts, rho_opts, theta_opts
 
 
@@ -53,7 +66,7 @@ dumpfreq = int(tmax / (2*dt))
 # ---------------------------------------------------------------------------- #
 
 # Domain -- 3D volume mesh
-degrees = [(1, 1), (1,2), (2,1), (2,2)]
+degrees = [(0, 0), (1, 0), (0, 1)]
 for degree in degrees:
     h_degree = degree[0]
     v_degree = degree[1]
@@ -65,9 +78,9 @@ for degree in degrees:
         nlayers = nlayers / 2
     m = PeriodicIntervalMesh(columns, L)
     mesh = ExtrudedMesh(m, layers=nlayers, layer_height=H/nlayers)
-    domain = Domain(mesh, dt, "CG", 
-                    horizontal_degree=h_degree, 
-		            vertical_degree=v_degree)
+    domain = Domain(mesh, dt, "CG",
+                    horizontal_degree=h_degree,
+                    vertical_degree=v_degree)
     # Equation
     Tsurf = 300.
     parameters = CompressibleParameters()
@@ -84,36 +97,44 @@ for degree in degrees:
                               dumpfreq=dumpfreq,
                               dumplist=['u'],
                               dump_nc=True,
-                              dump_vtus = False)
-    
-    diagnostic_fields = [CourantNumber(), ZonalComponent('u'), MeridionalComponent('u'),
-                         RadialComponent('u'), Perturbation('theta'), Perturbation('rho'),
-                         CompressibleKineticEnergy('u'), PotentialEnergy(eqns), InternalEnergy(eqns),
-                         RichardsonNumber('theta', parameters.g/Tsurf)]
+                              dump_vtus=False)
+
+    diagnostic_fields = [CourantNumber(), XComponent('u'), YComponent('u'), ZComponent('u'),
+                         Perturbation('theta'), Perturbation('rho')]
+
     io = IO(domain, output, diagnostic_fields=diagnostic_fields)
     # Transport options
-    if degree ==(0,0):
-        u_opts, rho_opts, theta_opts = lowestorderspaces(domain)
+    recovery_spaces = ((0, 0), (0, 1), (1, 0))
 
-    if v_degree == 0:
-       u_opts, rho_opts, theta_opts = lowestorderspaces(domain, BoundaryMethod.taylor)
-   
+    if degree in recovery_spaces:
+        if v_degree == 0:
+            u_opts, rho_opts, theta_opts = RecoverySpaces(mesh, v_degree, h_degree, BC=BoundaryMethod.taylor)
+        else:
+            u_opts, rho_opts, theta_opts = RecoverySpaces(mesh, v_degree, h_degree)
 
-    theta_opts = EmbeddedDGOptions()
-    transported_fields = [SSPRK3(domain, "u"),
-                            SSPRK3(domain, "rho"),
-                            SSPRK3(domain, "theta", options=theta_opts)]
-    transport_methods = [DGUpwind(eqns, "u"),
-                            DGUpwind(eqns, "rho"),
-                            DGUpwind(eqns, "theta", ibp=theta_opts.ibp)]
+        transported_fields = [SSPRK3(domain, "u", options=u_opts),
+                              SSPRK3(domain, "rho", options=rho_opts),
+                              SSPRK3(domain, "theta", options=theta_opts)]
+        transport_methods = [DGUpwind(eqns, "u"),
+                             DGUpwind(eqns, "rho"),
+                             DGUpwind(eqns, "theta")]
+    else:
+
+        theta_opts = EmbeddedDGOptions()
+        transported_fields = [SSPRK3(domain, "u"),
+                              SSPRK3(domain, "rho"),
+                              SSPRK3(domain, "theta", options=theta_opts)]
+        transport_methods = [DGUpwind(eqns, "u"),
+                             DGUpwind(eqns, "rho"),
+                             DGUpwind(eqns, "theta")]
 
     # Linear solver
     linear_solver = CompressibleSolver(eqns)
 
     # Time stepper
     stepper = SemiImplicitQuasiNewton(eqns, io, transported_fields,
-                                    transport_methods,
-                                    linear_solver=linear_solver)
+                                      transport_methods,
+                                      linear_solver=linear_solver)
 
     # ---------------------------------------------------------------------------- #
     # Initial conditions
