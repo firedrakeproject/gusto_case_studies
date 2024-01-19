5124b73143cb625989f6fbec8458a751b501360b
Witt-D <dw603@exeter.ac.uk>
2024-01-19 14:48:31 +0000

    implemented toms suggestions

--- compressible/skamarock_nonhydrostatic.py
+++ compressible/skamarock_nonhydrostatic.py
@@ -18,7 +18,7 @@ def RecoverySpaces(mesh, vertical_degree, horizontal_degree, BC=None):
     DG_hori_ele = FiniteElement('DG', cell, horizontal_degree+1, variant='equispaced')
     DG_vert_ele = FiniteElement('DG', interval, vertical_degree+1, variant='equispaced')
     CG_hori_ele = FiniteElement('CG', cell, horizontal_degree+1)
-    CG_vert_ele = FiniteElement('CG', interval, vertical_degree+2)
+    CG_vert_ele = FiniteElement('CG', interval, vertical_degree+1)
 
     VDG_ele = TensorProductElement(DG_hori_ele, DG_vert_ele)
     VCG_ele = TensorProductElement(CG_hori_ele, CG_vert_ele)
@@ -26,8 +26,8 @@ def RecoverySpaces(mesh, vertical_degree, horizontal_degree, BC=None):
     VCG = FunctionSpace(mesh, VCG_ele)
 
     # VR space for u transport
-    Vrh_hori_ele = FiniteElement('DG', cell, horizontal_degree+2)
-    Vrh_vert_ele = FiniteElement('CG', interval, vertical_degree+1)
+    Vrh_hori_ele = FiniteElement('DG', cell, horizontal_degree+1)
+    Vrh_vert_ele = FiniteElement('CG', interval, vertical_degree+2)
 
     Vrv_hori_ele = FiniteElement('CG', cell, horizontal_degree+2)
     Vrv_vert_ele = FiniteElement('DG', interval, horizontal_degree+1)
@@ -39,11 +39,11 @@ def RecoverySpaces(mesh, vertical_degree, horizontal_degree, BC=None):
     Vu_VR = FunctionSpace(mesh, Vrh_ele)
 
     # Vh space for u transport
-    VHh_hori_ele = FiniteElement('CG', quadrilateral, horizontal_degree+1)
+    VHh_hori_ele = FiniteElement('CG', cell, horizontal_degree+2)
     VHh_vert_ele = FiniteElement('DG', interval, vertical_degree+1)
 
-    VHv_hori_ele = FiniteElement('DG', quadrilateral, horizontal_degree+1)
-    VHv_vert_ele = FiniteElement('CG', quadrilateral, horizontal_degree+1)
+    VHv_hori_ele = FiniteElement('DG', cell, horizontal_degree+1)
+    VHv_vert_ele = FiniteElement('CG', interval, horizontal_degree+2)
 
     VHh_ele = HDiv(TensorProductElement(VHh_hori_ele, VHh_vert_ele))
     VHv_ele = HDiv(TensorProductElement(VHv_hori_ele, VHv_vert_ele))
@@ -52,12 +52,16 @@ def RecoverySpaces(mesh, vertical_degree, horizontal_degree, BC=None):
     Vu_VH = FunctionSpace(mesh, VHh_ele)
 
 
-    u_opts = RecoveryOptions(embedding_space=Vu_VR,
-                             recovered_space=Vu_VH,
-                             boundary_method=BC)
+    u_opts = RecoveryOptions(embedding_space=Vu_VH,
+                             recovered_space=Vu_VR,
+                             injection_method='recover',
+                             project_high_method='project',
+                             project_low_method='project',
+                             broken_method='project'
+                             )
     rho_opts = RecoveryOptions(embedding_space=VDG,
                                recovered_space=VCG,
-                               boundary_method=BC)
+                               )
     theta_opts = RecoveryOptions(embedding_space=VDG,
                                  recovered_space=VCG)
 
@@ -75,7 +79,7 @@ H = 1.0e4  # Height position of the model top
 nlayers = 10
 columns = 150
 tmax = 3600
-dumpfreq = int(tmax / (2*dt))
+dumpfreq = int(tmax / (4*dt))
 
 # ---------------------------------------------------------------------------- #
 # Set up model objects
@@ -108,14 +112,14 @@ for degree in degrees:
     points_x = np.linspace(0., L, 100)
     points_z = [H/2.]
     points = np.array([p for p in itertools.product(points_x, points_z)])
-    dirname = f'gravwave={h_degree}_v_order={v_degree}'
+    dirname = f'gravwave_recovery={h_degree}_v_order={v_degree}'
     output = OutputParameters(dirname=dirname,
                               dumpfreq=dumpfreq,
                               dumplist=['u'],
                               dump_nc=True,
                               dump_vtus=False)
 
-    diagnostic_fields = [CourantNumber(), XComponent('u'), YComponent('u'), ZComponent('u'),
+    diagnostic_fields = [CourantNumber(), XComponent('u'), ZComponent('u'),
                          Perturbation('theta'), Perturbation('rho')]
 
     io = IO(domain, output, diagnostic_fields=diagnostic_fields)
