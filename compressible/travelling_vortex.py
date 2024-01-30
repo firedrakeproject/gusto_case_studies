8188e14787e0690e355903a4c4829572035f2afc
Witt-D <dw603@exeter.ac.uk>
2024-01-30 11:24:43 +0000

    vortext test case

--- compressible/travelling_vortex.py
+++ compressible/travelling_vortex.py
@@ -3,12 +3,12 @@ The Travelling Vortex test case in a three dimensional channel
 """
 
 from gusto import *
-from firedrake import (PeriodicRectangleMesh, ExtrudedMesh, SpatialCoordinate, 
-                       Function, conditional, sqrt, as_vector, atan_2)
+from firedrake import (PeriodicIntervalMesh, ExtrudedMesh, SpatialCoordinate, 
+                       Function, conditional, sqrt, as_vector, atan2)
 
 from gusto.diagnostics import (CompressibleAbsoluteVorticity, 
                                CompressibleRelativeVorticity)
-from gusto.thermodynamics import theta
+
 
 
 tmax = 100
@@ -16,33 +16,32 @@ dt = 1
 dumpfreq = 25
 degree = (1, 1)
 
-Lx = 10000 # length
-Ly = 5000 # width
-Lz = 10000  # height
-dx = Lx / 32
-dy = Ly / 8
-dz = Lz / 32
-nlayers = int(Lz / dz)
-ncolumnsx = int(Lx / dx)
-ncolmunsy = int(Ly / dy)
+uc = 100.
+vc = 100.
+L = 10000.
+H = 10000.
 
-# Domain
-m = PeriodicRectangleMesh(ncolumnsx, ncolmunsy, Lx, Ly, 'both', quadrilateral=True)
-mesh = ExtrudedMesh(m, layers=nlayers, layer_height=dz)
-domain = Domain(mesh, dt, 'RTCF',  horizontal_degree=degree[0], 
+
+t = 0.
+delx = 100.
+nx = int(L/delx)
+
+m = PeriodicIntervalMesh(nx, L)
+mesh = ExtrudedMesh(m, layers=nx, layer_height=delx, periodic=True)
+x, y = SpatialCoordinate(mesh)
+domain = Domain(mesh, dt, 'CG',  horizontal_degree=degree[0], 
                 vertical_degree=degree[1])
 
 
 # Equations
-params = CompressibleParameters()
+params = CompressibleParameters(g=0)
 eqns = CompressibleEulerEquations(domain, params)
 print(f'ideal number of processors = {eqns.X.function_space().dim() / 50000}')
 
 #I/O
 dirname='traveling_vortex'
 output = OutputParameters(dirname=dirname, dumpfreq=dumpfreq, dump_nc=True)
-diagnostics = [CompressibleAbsoluteVorticity, CompressibleRelativeVorticity, 
-               XComponent('u'), ZComponent('u'), YComponent('u')]
+diagnostics = [XComponent('u'), ZComponent('u'),  Pressure(eqns)]
 io = IO(domain, output, diagnostic_fields=diagnostics)
 
 #Transport options
@@ -66,7 +65,6 @@ stepper = SemiImplicitQuasiNewton(eqns, io, transported_fields,
 u = stepper.fields('u')
 rho = stepper.fields('rho')
 theta = stepper.fields('theta')
-x, y, z = SpatialCoordinate(mesh)
 
 # spaces
 Vu = u.function_space()
@@ -76,38 +74,66 @@ Vr = rho.function_space()
 # constants
 kappa = params.kappa
 p_0 = params.p_0
-xc = Lx / 2
-yc = Ly / 2
-zc = Lz / 2 
-R = 2500 #scaling
-r = sqrt(x**2 + y**2 + z**2)
-u0 = 100
-v0 = 0
-w0 = 0
-T = 270
+xc = L / 2  
+yc = L / 2 
 
-phi = atan_2((z - zc),(x - xc))
-u_r_expr = -1024 * sin(phi) * (1*r**2)**6 * r**6 + u0
-w_r_expr = 1024 * sin(phi) * (1*r**2)**6 * r**6 + w0
+R = 0.4*L # radius of voertex
+r = sqrt(x**2 + y**2) / R
+phi = atan2((y - yc),(x - xc))
 
-p_r_expr = -1/72*r**36 + 6/35*r**35 - 15/17*r**34 + 74/33*r**33- 57/32*r**32 - 174/31*r**31 +\
-          269/15 *r**30 - 450/29*r**29 - 153/8*r**28 + 1564/27*r**27 - 510/13*r**26 -\
-          204/5*r**25 + 737/8*r**24 - 1032/23*r**23 - 477/11*r**22 + 64*r**21 -\
-          81/40*r**20 -1242/19*r**19 + 731/9*r**18 - 966/17*r**17 + 219/8*r**16 -\
-          146/15*r**15 + 18/7*r**14 - 6/13*r**13 + 1/24*r**12
+# rho expression
+rhoc = 1
 rho_r_expr = 1 - 1/2*(1 - r**2)**6
+rho0 = conditional(r>=1, rhoc, rho_r_expr)
 
-u_expr = conditional(r < R, u_r_expr, u0)
-w_expr = conditional(r< R, w_r_expr, w0)
-p_expr = conditional(r < R, p_r_expr, 0)
-rho_expr = conditional(r < R, rho_r_expr, 0)
-U_expr = as_vector([u_expr, v0, w_expr])
-theta_expr = T*(p_0 / p_expr)**kappa
+# u expression
+u_cond = (1024 * (1.0 - r)**6 * r**6 )
+ux_expr = conditional(r>=1, uc, uc + u_cond * (-(y-yc)/(r*R))) 
+uy_expr = conditional(r>=1, vc, vc + u_cond * (-(x-xc)/(r*R))) 
+u0 = as_vector([ux_expr, uy_expr])
 
-u.project(U_expr)
-rho.interpolate(rho_expr)
-theta.interpolate(theta_expr)
-compressible_hydrostatic_balance(eqns, theta, rho, solve_for_rho=True)
+#pressure and theta expressions
+coe = np.zeros((25))
+coe[0]  =     1.0 / 24.0
+coe[1]  = -   6.0 / 13.0
+coe[2]  =    18.0 /  7.0
+coe[3]  = - 146.0 / 15.0
+coe[4]  =   219.0 / 8.0
+coe[5]  = - 966.0 / 17.0
+coe[6]  =   731.0 /  9.0
+coe[7]  = -1242.0 / 19.0
+coe[8]  = -  81.0 / 40.0
+coe[9]  =   64.
+coe[10] = - 477.0 / 11.0
+coe[11] = -1032.0 / 23.0
+coe[12] =   737.0 / 8.0
+coe[13] = - 204.0 /  5.0
+coe[14] = - 510.0 / 13.0
+coe[15] =  1564.0 / 27.0
+coe[16] = - 153.0 /  8.0
+coe[17] = - 450.0 / 29.0
+coe[18] =   269.0 / 15.0
+coe[19] = - 174.0 / 31.0
+coe[20] = -  57.0 / 32.0
+coe[21] =    74.0 / 33.0
+coe[22] = -  15.0 / 17.0
+coe[23] =     6.0 / 35.0
+coe[24] =  -  1.0 / 72.0
+p0 = 0
+for i in range(25):
+    p0 += coe[i] * (r**(12+i)-1)
+mach = 0.341
+p0 = 1 + 1024**2 * mach *conditional(r>=1, 0, p0)
+Rd = params.R_d
+gamma=1.4
+pref=params.p_0
+
+T0 = p0 / (rho0*Rd)
+theta0 = T0*(pref / p0)**0.286
+u.project(u0)
+rho.interpolate(rho0)
+theta.interpolate(theta0)
+#compressible_hydrostatic_balance(eqns, theta, rho, solve_for_rho=True)
 
 # make mean fields
 print('make mean fields')
