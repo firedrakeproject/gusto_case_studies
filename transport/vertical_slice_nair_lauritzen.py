5cc1ab9bbd2266cbd93512ad093396453de30de6
Tom Bendall <thomas.bendall@metoffice.gov.uk>
2024-05-01 08:59:10 +0100

    try to get all tests running

--- transport/vertical_slice_nair_lauritzen.py
+++ transport/vertical_slice_nair_lauritzen.py
@@ -1,14 +1,11 @@
 from gusto import *
-from firedrake import PeriodicIntervalMesh, ExtrudedMesh, Constant, ge, le, exp, cos, \
-    sin, conditional, interpolate, SpatialCoordinate, VectorFunctionSpace, \
-    Function, assemble, dx, FunctionSpace, pi, min_value, acos, as_vector
-
-import numpy as np
+from firedrake import (PeriodicIntervalMesh, ExtrudedMesh, cos, exp,
+                       sin, SpatialCoordinate, pi, min_value, as_vector)
 
 u"""
 This script implements a the test given in the 'Charney-Phillips trilemma'
 paper by Bendall, Wood, Thuburn, and Cotter. This considers a planar version of
-the Gaussian test case given by Nair and Laurtizen. 
+the Gaussian test case given by Nair and Laurtizen.
 
 This tests a coupled transport equation for moisture.
 
@@ -19,9 +16,9 @@ Whereas the dry density obeys the conservative form:
 ∂/∂t (ρ_d) + ∇.(ρ_d*u) = 0
 
 There are two configurations that can be run:
-  The 'convergence' configuration has an initial condition of a linearly 
+  The 'convergence' configuration has an initial condition of a linearly
   varying density field and two Gaussian bumps for the mixing ratio.
-  The 'consistency' configuration has an initial condition of a 
+  The 'consistency' configuration has an initial condition of a
   constant mixing ratio and two Gaussian bumps for the density.
 
 """
@@ -48,6 +45,10 @@ columns = 200.  # number of columns
 dx = Lx/nlayers
 dz = Hz/columns
 
+# ---------------------------------------------------------------------------- #
+# Set up model objects
+# ---------------------------------------------------------------------------- #
+
 period_mesh = PeriodicIntervalMesh(columns, Lx)
 mesh = ExtrudedMesh(period_mesh, layers=nlayers, layer_height=Hz/nlayers)
 domain = Domain(mesh, dt, "CG", 1)
@@ -55,30 +56,30 @@ x,z = SpatialCoordinate(mesh)
 
 # Define the mixing ratio and density as tracers
 m_X = ActiveTracer(name='m_X', space=m_X_space,
-                 variable_type=TracerVariableType.mixing_ratio,
-                 transport_eqn=TransportEquationType.advective)
-                 
-rho_d = ActiveTracer(name='rho_d', space='DG',
-                 variable_type=TracerVariableType.density,
-                 transport_eqn=TransportEquationType.conservative)
+                   variable_type=TracerVariableType.mixing_ratio,
+                   transport_eqn=TransportEquationType.advective)
 
-tracers = [m_X,rho_d]
+rho_d = ActiveTracer(name='rho_d', space='DG',
+                     variable_type=TracerVariableType.density,
+                     transport_eqn=TransportEquationType.conservative)
+
+tracers = [m_X, rho_d]
 
 # Equation
 V = domain.spaces("HDiv")
-eqn = CoupledTransportEquation(domain, active_tracers=tracers, Vu = V)
+eqn = CoupledTransportEquation(domain, active_tracers=tracers, Vu=V)
 
 # I/O
-dirname = "vertical_slice_nair_lauritzen_diagtest"+case
+dirname = "vertical_slice_nair_lauritzen_"+case
 
 # Dump the solution at each day
 dumpfreq = int(100./dt)
 
 # Set dump_nc = True to use tomplot.
 output = OutputParameters(dirname=dirname,
-                          dumpfreq = dumpfreq,
-                          dump_nc = True,
-                          dump_vtus = False)
+                          dumpfreq=dumpfreq,
+                          dump_nc=True,
+                          dump_vtus=False)
 
 # Use a tracer density diagnostic to track conservation
 diagnostic_fields = [TracerDensity('m_X','rho_d')]
@@ -90,13 +91,24 @@ U = Lx/tmax
 W = U/10.
 
 def u_t(t):
-  xd = x - U*t
-  u = U - (W*pi*Lx/Hz)*cos(pi*t/tmax)*cos(2*pi*xd/Lx)*cos(pi*z/Hz)
-  w = 2*pi*W*cos(pi*t/tmax)*sin(2*pi*xd/Lx)*sin(pi*z/Hz)
-  
-  u_expr = as_vector((u,w))
-  
-  return u_expr
+    xd = x - U*t
+    u = U - (W*pi*Lx/Hz)*cos(pi*t/tmax)*cos(2*pi*xd/Lx)*cos(pi*z/Hz)
+    w = 2*pi*W*cos(pi*t/tmax)*sin(2*pi*xd/Lx)*sin(pi*z/Hz)
+
+    u_expr = as_vector((u,w))
+
+    return u_expr
+
+transport_scheme = SSPRK3(domain)
+transport_methods = [DGUpwind(eqn, "m_X"), DGUpwind(eqn, "rho_d")]
+
+# Time stepper
+stepper = PrescribedTransport(eqn, transport_scheme, io, transport_methods,
+                              prescribed_transporting_velocity=u_t)
+
+# ---------------------------------------------------------------------------- #
+# Initial conditions
+# ---------------------------------------------------------------------------- #
 
 # Specify locations of the two Gaussians
 xc1 = 5.*Lx/8.
@@ -106,51 +118,47 @@ xc2 = 3.*Lx/8.
 zc2 = Hz/2.
 
 def l2_dist(xc,zc):
-  return min_value(abs(x-xc), Lx-abs(x-xc))**2 + (z-zc)**2
+    return min_value(abs(x-xc), Lx-abs(x-xc))**2 + (z-zc)**2
 
 lc = 2.*Lx/25.
 m0 = 0.02
 
 # Set the initial states from the choice of configuration
 if case == 'convergence':
-  f0 = 0.05
-  
-  rho_t = 0.5
-  rho_b = 1.
-  
-  rho_d_0 = rho_b + z*(rho_t-rho_b)/Hz  
-  
-  g1 = f0*exp(-l2_dist(xc1,zc1)/(lc**2))
-  g2 = f0*exp(-l2_dist(xc2,zc2)/(lc**2))
-  
-  m_X_0 = m0 + g1 + g2
-  
+    f0 = 0.05
+
+    rho_t = 0.5
+    rho_b = 1.
+
+    rho_d_0 = rho_b + z*(rho_t-rho_b)/Hz
+
+    g1 = f0*exp(-l2_dist(xc1,zc1)/(lc**2))
+    g2 = f0*exp(-l2_dist(xc2,zc2)/(lc**2))
+
+    m_X_0 = m0 + g1 + g2
+
 elif case == 'consistency':
-  f0 = 0.5
-  rho_b = 0.5
-  
-  g1 = f0*exp(-l2_dist(xc1,zc1)/(lc**2))
-  g2 = f0*exp(-l2_dist(xc2,zc2)/(lc**2))
-  
-  rho_d_0 = rho_b + g1 + g2
-  
-  m_X_0 = m0 + 0*x
-  
+    f0 = 0.5
+    rho_b = 0.5
+
+    g1 = f0*exp(-l2_dist(xc1,zc1)/(lc**2))
+    g2 = f0*exp(-l2_dist(xc2,zc2)/(lc**2))
+
+    rho_d_0 = rho_b + g1 + g2
+
+    m_X_0 = m0 + 0*x
+
 else:
-  raise NotImplementedError('Specified case is not recognised.')
+    raise NotImplementedError('Specified case is not recognised.')
 
-
-transport_scheme = SSPRK3(domain)
-transport_methods = [DGUpwind(eqn, "m_X"), DGUpwind(eqn, "rho_d")]
-    
-# Time stepper
-stepper = PrescribedTransport(eqn, transport_scheme, io, transport_methods,
-                              prescribed_transporting_velocity=u_t)
-
-# initial conditions
+# Set fields
 stepper.fields("m_X").interpolate(m_X_0)
 stepper.fields("rho_d").interpolate(rho_d_0)
 u0 = stepper.fields("u")
 u0.project(u_t(0))
+
+# ------------------------------------------------------------------------ #
+# Run
+# ------------------------------------------------------------------------ #
 
 stepper.run(t=0, tmax=tmax)
