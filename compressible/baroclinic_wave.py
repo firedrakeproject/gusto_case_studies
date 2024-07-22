60f87bcdb71c37161e578900dcffc9bf80ee34ec
Witt-D <dw603@exeter.ac.uk>
2024-07-22 12:40:58 +0100

    removed old script added up to date one

--- compressible/baroclinic_wave.py
+++ compressible/baroclinic_wave.py
@@ -1,33 +1,8 @@
 """
 The deep atmosphere dry baroclinic wave Test case from Ullich et al. 2013 
 Script by Daniel Witt 
-Last Updated 16/10/2023
+Last Updated 22/07/2024
 
-7 Different configuration are provided for transport set up:
-
-Config 1: Lowest order finite element space, with vector advection form 
-          and no theta limiter
-Config 2: Lowest order finite element space with vector advection 
-          form and a DG1Limiter on theta
-Config 3: Next to lowest order space with vector advection form with SUPGOptions
-          for theta transport and no limiter
-Config 4: Next to lowest order space with vector advection form, EmbeddedDG for 
-          theta transport and no limiter
-Config 5: Next to lowest order space with vector advection form, EmbeddedDG for 
-          theta transport and a limiter on the theta field
-Config 6: Next to lowest order space with vector invariant form with SUPGOptions
-          for theta transport and no limiter
-Config 7: Next to lowest order space with vector invariant form, EmbeddedDG for 
-          theta transport and no limiter
-Config 8: Next to lowest order space with vector invariant form, EmbeddedDG for 
-          theta transport and a limiter on the theta field
-
-In addition to these configuration there are optional adjustments
-
-Perturbation:    The properties of the perturbation
-                 None: No perturbation 
-                 single: Velocity perturbation in the upper mid lattitude
-                 double: Symetrical Perturbations in the N/S hemispheres
 Variable height: Applies a non uniform height field.  
                  Default = True
 Alpha:           Adjusts the ratio of implicit to explicit in the solver. 
@@ -38,135 +13,21 @@ from firedrake import (ExtrudedMesh,  TensorProductElement,
                        SpatialCoordinate, cos, sin, pi, sqrt, HDiv, HCurl,
                        exp, Constant, Function, as_vector, acos, interval,
                        errornorm, norm, min_value, max_value, le, ge, FiniteElement,
-                       NonlinearVariationalProblem, NonlinearVariationalSolver)
-from gusto import *                                            #
+                       NonlinearVariationalProblem, NonlinearVariationalSolver, Citations)
+from gusto import *    
 # --------------------------------------------------------------#
 # Configuratio Options
 # -------------------------------------------------------------- #
-config = 'config4'
+
+order = (1, 1)
+c = 16 # number of cells per cube face of cubed sphere
+nlayers = 15
 dt = 900.
 days = 15.
 tmax = days * 24. * 60. * 60.
-n = 24   # cells per cubed sphere face edge
-nlayers = 15 # vertical layers
 alpha = 0.50 # ratio between implicit and explict in solver
 variable_height = True
-perturbed = True
-perturbation = 'single'
-
-
-if perturbed:
-    if perturbation == 'single':
-        location = [(pi/9, 2*pi/9)]
-    elif perturbation == 'double':
-        location = [(pi/9, 2*pi/9), (pi/9, -2*pi/9)]
-    else:
-        raise ValueError('Please select a valid perturbation option')
-
-
-# Lowest Order Configs
-if config == 'config1':   # lowest order no limiter
-    DGdegree = 0
-    u_form = 'vector_advection_form'
-    transport_name = 'recovered'
-    limited = False
-    n = n*2
-    nlayers = nlayers*2
-
-elif config == 'config2': # lowest order theta limited
-    DGdegree = 0
-    u_form = 'vector_advection_form'
-    transport_name = 'recovered'
-    limited = True
-    n = n*2
-    nlayers = nlayers*2
-
-# Vector advection form options
-elif config =='config3': # vector advection SUPG 
-    DGdegree = 1
-    u_form = 'vector_advection_form'
-    theta_transport = SUPGOptions()
-    transport_name = 'SUPG'
-    limited = False
-
-elif config =='config4': # Vector advection embedded not limited
-    DGdegree = 1
-    u_form = 'vector_advection_form' 
-    theta_transport = EmbeddedDGOptions()
-    transport_name = 'embedded'
-    limited = False
-
-elif config =='config5': # Vector advection embedded theta limited
-    DGdegree = 1
-    u_form = 'vector_advection_form'
-    theta_transport = EmbeddedDGOptions()
-    transport_name = 'embedded'
-    limited = True
-# Vector invariant options
-elif config =='config6': # vector invariant SUPG 
-    DGdegree = 1
-    u_form = 'vector_invariant_form'
-    theta_transport = SUPGOptions()
-    transport_name = 'SUPG'
-    limited = False
-
-elif config =='config7': # vector invariant embedded not limited
-    DGdegree = 1
-    u_form = 'vector_invariant_form'
-    theta_transport = EmbeddedDGOptions()
-    transport_name = 'embedded'
-    limited = False
-
-elif config =='config8': # vector invariant embedded theta limited 
-    DGdegree = 1
-    u_form = 'vector_invariant_form'
-    theta_transport = EmbeddedDGOptions()
-    transport_name = 'embedded'
-    limited = True
-# -------------------------------------------------------------- #
-# Script Options
-# -------------------------------------------------------------- #
-
-dirname = f'{config}_{perturbation}_wave_n={n}_dt={dt}'
-
-if variable_height:
-    dirname = f'{dirname}_varied_height'
-
-if limited:
-    dirname = f'{dirname}_limited' 
-if not perturbed:
-    dirname = f'SBR_{config}_{u_form}_{transport_name}'
-
-# function for building lowest order function spaces
-def buildUrecoverySpaces(mesh, degree):
-    # horizontal base spaces
-    hdiv_family = "RTCF" 
-    hcurl_family = "RTCE"
-
-    cell = mesh._base_mesh.ufl_cell().cellname()
-    u_div_hori = FiniteElement(hdiv_family, cell, degree + 1)
-    w_div_hori = FiniteElement("DG", cell, degree)
-    u_curl_hori = FiniteElement(hcurl_family, cell, degree+1)
-    w_curl_hori = FiniteElement("CG", cell, degree + 1)
-
-    # vertical base spaces
-    u_div_vert = FiniteElement("DG", interval, degree)
-    w_div_vert = FiniteElement("CG", interval, degree + 1)
-    u_curl_vert = FiniteElement("CG", interval, degree + 1)
-    w_curl_vert = FiniteElement("DG", interval, degree)
-
-    # build elements
-    u_div_element = HDiv(TensorProductElement(u_div_hori, u_div_vert))
-    w_div_element = HDiv(TensorProductElement(w_div_hori, w_div_vert))
-    u_curl_element = HCurl(TensorProductElement(u_curl_hori, u_curl_vert))
-    w_curl_element = HCurl(TensorProductElement(w_curl_hori, w_curl_vert))
-    hdiv_element = u_div_element + w_div_element
-    hcurl_element = u_curl_element + w_curl_element
-
-    VHDiv = FunctionSpace(mesh, hdiv_element)
-    VHCurl = FunctionSpace(mesh, hcurl_element)
-
-    return VHDiv, VHCurl
+u_form = 'vector_advection_form'
 
 # -------------------------------------------------------------- #
 # Set up Model
@@ -174,13 +35,18 @@ def buildUrecoverySpaces(mesh, degree):
 # Domain
 a = 6.371229e6  # radius of earth
 ztop = 3.0e4  # max height
+dirname = f'baroclinic_wave_c={c}_dt={dt}_{u_form}_order={order[0]}_{order[1]}'
 
 if variable_height == True: 
     layerheight=[]
     runningheight=0
     # Calculating Non-uniform height field
     for m in range(1,nlayers+1):
-        mu = 8
+        mu = 3
+        if nlayers == 15:
+            mu = 15
+        if nlayers == 30:
+            mu = 15
         height = ztop * ((mu * (m / nlayers)**2 + 1)**0.5 - 1) / ((mu + 1)**0.5 - 1)
         width = height - runningheight
         runningheight = height
@@ -188,115 +54,52 @@ if variable_height == True:
 else: 
     layerheight = ztop / nlayers
 
-base_mesh = GeneralCubedSphereMesh(a, num_cells_per_edge_of_panel=n, degree=2)
+base_mesh = GeneralCubedSphereMesh(a, num_cells_per_edge_of_panel=c, degree=2)
 mesh = ExtrudedMesh(base_mesh, layers=nlayers, layer_height=layerheight, extrusion_type='radial')
-xyz= SpatialCoordinate(mesh)
-lon, lat, _ = lonlatr_from_xyz(xyz[0], xyz[1], xyz[2])
-domain = Domain(mesh, dt, "RTCF", degree=DGdegree)
+domain = Domain(mesh, dt, "RTCF", horizontal_degree=order[0], vertical_degree=order[1])
 
 # Equations
 params = CompressibleParameters()
 omega = Constant(7.292e-5)
-Omega = as_vector((0, 0, omega))
+Omega = as_vector(0, 0, omega)
 print('making eqn')    
-eqn = CompressibleEulerEquations(domain, params, Omega=Omega, u_transport_option=u_form)
-print(f'Number of DOFs = {eqn.X.function_space().dim()}')
+eqn = CompressibleEulerEquations(domain, params, Omega=Omega u_transport_option=u_form)
+print(f'Optimal Cores = {eqn.X.function_space().dim() / 50000}')
 
 output = OutputParameters(dirname=dirname,
-                          dumpfreq=12, # every 3 hours
-                          dump_nc=True,
-                          dump_vtus=False)
-diagnostic_fields = [MeridionalComponent('u'), ZonalComponent('u'),RadialComponent('u'),
-                    CourantNumber(), Temperature(eqn), Gradient('Temperature'), 
-                    MeridionalComponent('Temperature_gradient'), ZonalComponent('Temperature_gradient'),
-                    Pressure(eqn), SteadyStateError('Pressure_Vt'), 
-                    CompressibleKineticEnergy(), PotentialEnergy(eqn), InternalEnergy(eqn)]
-          
+                        dumpfreq=(3600 * 8) / dt, # every 8 hours
+                        dump_nc=True,
+                        dump_vtus=False)
+diagnostic_fields = []
 io = IO(domain, output, diagnostic_fields=diagnostic_fields)
-
-# Transport Schemes 
-
-if DGdegree == 0:
-
-    VDG1 = domain.spaces('DG1_equispaced')
-    VCG1 = FunctionSpace(mesh, 'CG', 1)
-    VHDiv1, VHcurl = buildUrecoverySpaces(mesh, 1)
-    VHDiv0, VHcurl0 = buildUrecoverySpaces(mesh, 0)
-    if limited:
-        limiter = DG1Limiter(VDG1)
-    else:
-        limiter = None
-
-
-    u_opts = RecoveryOptions(embedding_space=VHDiv1,
-                            recovered_space=VHcurl0,
-                            boundary_method=BoundaryMethod.hcurl, # try on 1 if doesnt work
-                            injection_method = 'project',
-                            project_high_method = 'project',
-                            broken_method='project') 
-
-    rho_opts = RecoveryOptions(embedding_space=VDG1,
-                            recovered_space=VCG1,
-                            boundary_method=BoundaryMethod.extruded)
-
-    theta_opts = RecoveryOptions(embedding_space=VDG1,
-                                recovered_space=VCG1)
-    
-    transported_fields = []
-    transported_fields.append(SSPRK3(domain, "u", options=u_opts))
-    transported_fields.append(SSPRK3(domain, "rho", options=rho_opts))
-    transported_fields.append(SSPRK3(domain, "theta", options=theta_opts, limiter=limiter))
-
-    transport_methods = [DGUpwind(eqn, 'u'),
-                        DGUpwind(eqn, 'rho'),
-                        DGUpwind(eqn, 'theta')]
-else:
-    if limited:
-        Vtheta = domain.spaces("theta")
-        limiter = ThetaLimiter(Vtheta)
-    else:
-        limiter = None
-
-    if isinstance(theta_transport, SUPGOptions):
-        theta_ibp = theta_transport.ibp
-    else:
-        theta_ibp = IntegrateByParts.ONCE
-    
-    transported_fields = []
-    transport_methods = []
-    if u_form == 'vector_invariant_form':
-        transported_fields.append(TrapeziumRule(domain, "u"))
-        transport_methods.append(DGUpwind(eqn, 'u'))
-    else:
-        transported_fields.append(TrapeziumRule(domain, "u", options=SUPGOptions()))
-        transport_methods.append(DGUpwind(eqn, 'u', ibp=SUPGOptions().ibp))
-    
-    transported_fields.append(SSPRK3(domain, "rho"))
-    transported_fields.append(SSPRK3(domain, "theta", options=theta_transport, limiter=limiter))
-
-    transport_methods.append(DGUpwind(eqn, 'rho'))
-    transport_methods.append(DGUpwind(eqn, 'theta', ibp=theta_ibp))
+# Transport options
+theta_opts = EmbeddedDGOptions()
+transported_fields = [SSPRK3(domain, "u"),
+                    SSPRK3(domain, "rho"),
+                    SSPRK3(domain, "theta", options=theta_opts)]
+transport_methods = [DGUpwind(eqn, "u"),
+                    DGUpwind(eqn, "rho"),
+                    DGUpwind(eqn, "theta")]
 
 # Linear Solver
 linear_solver = CompressibleSolver(eqn, alpha=alpha)
 
 # Time Stepper
 stepper = SemiImplicitQuasiNewton(eqn, io, transported_fields,
-                                  transport_methods,
-                                  linear_solver=linear_solver, alpha=alpha)
+                                transport_methods,
+                                linear_solver=linear_solver, alpha=alpha,
+                                num_outer=4, num_inner=1)
 
 # -------------------------------------------------------------- #
 # Initial Conditions
 # -------------------------------------------------------------- #
+xyz= SpatialCoordinate(mesh)
+lon, lat, _ = lonlatr_from_xyz(xyz[0], xyz[1], xyz[2])
 x = xyz[0]
 y = xyz[1]
 z = xyz[2]
 r = sqrt(x**2 + y**2 + z**2)
 l = sqrt(x**2 + y**2)
-unsafe_x = x / l
-unsafe_y = y / l
-safe_x = min_value(max_value(unsafe_x, -1), 1)
-safe_y = min_value(max_value(unsafe_y, -1), 1)
 
 # set up parameters
 Rd = params.R_d
@@ -311,6 +114,7 @@ T0 = 0.5 * (T0e + T0p)
 H = Rd * T0 / g # scale height of atmosphere
 k = 3 # power of temp field
 b = 2 # half width parameter
+Vp = 1 # maximum velocity of perturbation
 
 u0 = stepper.fields("u")
 rho0 = stepper.fields("rho")
@@ -354,63 +158,32 @@ rho_expr = P_expr / (Rd * Temp)
 # Perturbation
 # -------------------------------------------------------------- #
 
+base_zonal_u = wind 
+base_merid_u = Constant(0.0) 
 
-def VelocityPerturbation(base_state, location, mesh, Vp=1):
-    '''
-    A function which applies a velocity perturbation in the zonal and merirdioanl velocity 
-    fields
-    args:
-        wind: The current velocity state : type: Tuple of zonal, meridional and radial fields
-        location: Location of perturbation: type: tuple of co-ordinates in lat / lon
-        mesh: requires the mesh to calculate lat, lon, r
-        r: Location vector
-    Optional: 
-        Vp: Maximum velocity of perturbation
-    '''
+a = 6.371229e6
+zt = 1.5e4     # top of perturbation
+d0 = a / 6     # horizontal radius of perturbation
+a = 6.371229e6   
+err_tol = 1e-12
+lon_c , lat_c = pi/9, 2*pi/9 # location of perturbation centre
 
-    xyz = SpatialCoordinate(mesh)
-    lon, lat,  _ = lonlatr_from_xyz(xyz[0], xyz[1], xyz[2])
-    r = sqrt(xyz[0]**2 + xyz[1]**2 + xyz[2]**2)
-    a = 6.371229e6
-    zt = 1.5e4     # top of perturbation
-    d0 = a / 6     # horizontal radius of perturbation
-    zonal_u, merid_u, _ = base_state
-    a = 6.371229e6   
-    err_tol = 1e-12
-    
-    for co_ords in location:
-        lon_c , lat_c = co_ords # location of perturbation centre
-        d = a * acos(sin(lat_c)*sin(lat) + cos(lat_c)*cos(lat)*cos(lon - lon_c)) # distance from centre of perturbation
+d = a * acos(sin(lat_c)*sin(lat) + cos(lat_c)*cos(lat)*cos(lon - lon_c)) # distance from centre of perturbation
 
-        depth = r - a # The distance from origin subtracted from earth radius
-        zeta = conditional(ge(depth,zt-err_tol), 0, 1 - 3*(depth / zt)**2 + 2*(depth / zt)**3) # peturbation vertical taper
+depth = r - a # The distance from origin subtracted from earth radius
+zeta = conditional(ge(depth,zt-err_tol), 0, 1 - 3*(depth / zt)**2 + 2*(depth / zt)**3) # peturbation vertical taper
 
-        perturb_magnitude = (16*Vp/(3*sqrt(3))) * zeta * sin((pi * d) / (2 * d0)) * cos((pi * d) / (2 * d0)) ** 3
+perturb_magnitude = (16*Vp/(3*sqrt(3))) * zeta * sin((pi * d) / (2 * d0)) * cos((pi * d) / (2 * d0)) ** 3
 
 
-        zonal_pert = conditional(le(d,err_tol), 0, 
-                                conditional(ge(d,(d0-err_tol)), 0, -perturb_magnitude * (-sin(lat_c)*cos(lat) + cos(lat_c)*sin(lat)*cos(lon - lon_c)) / sin(d / a)))
-        meridional_pert = conditional(le(d,err_tol), 0, 
-                                    conditional(ge(d,d0-err_tol), 0, perturb_magnitude * cos(lat_c)*sin(lon - lon_c) / sin(d / a)))
+zonal_pert = conditional(le(d,err_tol), 0, 
+                        conditional(ge(d,(d0-err_tol)), 0, -perturb_magnitude * (-sin(lat_c)*cos(lat) + cos(lat_c)*sin(lat)*cos(lon - lon_c)) / sin(d / a)))
+meridional_pert = conditional(le(d,err_tol), 0, 
+                            conditional(ge(d,d0-err_tol), 0, perturb_magnitude * cos(lat_c)*sin(lon - lon_c) / sin(d / a)))
 
-        zonal_u = zonal_u + zonal_pert
-        merid_u = merid_u + meridional_pert
-    return (zonal_u, merid_u)
-
-# -------------------------------------------------------------- #
-# Configuring fields
-# -------------------------------------------------------------- #
-# get components of u in spherical polar coordinates
-
-zonal_u = wind 
-merid_u = Constant(0.0) 
+zonal_u = base_zonal_u + zonal_pert
+merid_u = base_merid_u + meridional_pert
 radial_u = Constant(0.0)
-
-base_state = (zonal_u, merid_u, radial_u)
-
-if perturbed == True:
-    zonal_u, merid_u, = VelocityPerturbation(base_state, location, mesh)
-
 # Get spherical basis vectors, expressed in terms of thier (x,y,z) components:
 e_lon = xyz_vector_from_lonlatr(1, 0, 0, xyz)
 e_lat = xyz_vector_from_lonlatr(0, 1, 0, xyz)
@@ -447,5 +220,5 @@ theta_b = Function(Vt).assign(theta0)
 # assign reference profiles
 stepper.set_reference_profiles([('rho', rho_b),
                                 ('theta', theta_b)])
-print('Intialise Windy Boi')
+
 stepper.run(t=0, tmax=tmax)
