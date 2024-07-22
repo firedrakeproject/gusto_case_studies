abc9921f12f1fe84c2486c8b07cd648d0d91d07c
Witt-D <dw603@exeter.ac.uk>
2024-07-22 13:42:19 +0100

    flake8

--- compressible/baroclinic_wave.py
+++ compressible/baroclinic_wave.py
@@ -1,31 +1,29 @@
 """
-The deep atmosphere dry baroclinic wave Test case from Ullich et al. 2013 
-Script by Daniel Witt 
+The deep atmosphere dry baroclinic wave Test case from Ullich et al. 2013
+Script by Daniel Witt
 Last Updated 22/07/2024
 
-Variable height: Applies a non uniform height field.  
+Variable height: Applies a non uniform height field.
                  Default = True
-Alpha:           Adjusts the ratio of implicit to explicit in the solver. 
+Alpha:           Adjusts the ratio of implicit to explicit in the solver.
                  Default = 0.5
 """
 
-from firedrake import (ExtrudedMesh,  TensorProductElement,
-                       SpatialCoordinate, cos, sin, pi, sqrt, HDiv, HCurl,
-                       exp, Constant, Function, as_vector, acos, interval,
-                       errornorm, norm, min_value, max_value, le, ge, FiniteElement,
-                       NonlinearVariationalProblem, NonlinearVariationalSolver, Citations)
-from gusto import *    
+from firedrake import (ExtrudedMesh, SpatialCoordinate, cos, sin, pi, sqrt,
+                       exp, Constant, Function, as_vector, acos, errornorm, norm,
+                       le, ge, NonlinearVariationalProblem, NonlinearVariationalSolver)
+from gusto import *
 # --------------------------------------------------------------#
 # Configuratio Options
 # -------------------------------------------------------------- #
 
 order = (1, 1)
-c = 16 # number of cells per cube face of cubed sphere
+c = 16  # number of cells per cube face of cubed sphere
 nlayers = 15
 dt = 900.
 days = 15.
 tmax = days * 24. * 60. * 60.
-alpha = 0.50 # ratio between implicit and explict in solver
+alpha = 0.50  # ratio between implicit and explict in solver
 variable_height = True
 u_form = 'vector_advection_form'
 
@@ -37,11 +35,11 @@ a = 6.371229e6  # radius of earth
 ztop = 3.0e4  # max height
 dirname = f'baroclinic_wave_c={c}_dt={dt}_{u_form}_order={order[0]}_{order[1]}'
 
-if variable_height == True: 
-    layerheight=[]
-    runningheight=0
+if variable_height is True:
+    layerheight = []
+    runningheight = 0
     # Calculating Non-uniform height field
-    for m in range(1,nlayers+1):
+    for m in range(1, nlayers+1):
         mu = 3
         if nlayers == 15:
             mu = 15
@@ -51,7 +49,7 @@ if variable_height == True:
         width = height - runningheight
         runningheight = height
         layerheight.append(width)
-else: 
+else:
     layerheight = ztop / nlayers
 
 base_mesh = GeneralCubedSphereMesh(a, num_cells_per_edge_of_panel=c, degree=2)
@@ -62,38 +60,38 @@ domain = Domain(mesh, dt, "RTCF", horizontal_degree=order[0], vertical_degree=or
 params = CompressibleParameters()
 omega = Constant(7.292e-5)
 Omega = as_vector(0, 0, omega)
-print('making eqn')    
-eqn = CompressibleEulerEquations(domain, params, Omega=Omega u_transport_option=u_form)
+print('making eqn')
+eqn = CompressibleEulerEquations(domain, params, Omega=Omega, u_transport_option=u_form)
 print(f'Optimal Cores = {eqn.X.function_space().dim() / 50000}')
 
 output = OutputParameters(dirname=dirname,
-                        dumpfreq=(3600 * 8) / dt, # every 8 hours
-                        dump_nc=True,
-                        dump_vtus=False)
+                          dumpfreq=(3600 * 8) / dt,  # every 8 hours
+                          dump_nc=True,
+                          dump_vtus=False)
 diagnostic_fields = []
 io = IO(domain, output, diagnostic_fields=diagnostic_fields)
 # Transport options
 theta_opts = EmbeddedDGOptions()
 transported_fields = [SSPRK3(domain, "u"),
-                    SSPRK3(domain, "rho"),
-                    SSPRK3(domain, "theta", options=theta_opts)]
+                      SSPRK3(domain, "rho"),
+                      SSPRK3(domain, "theta", options=theta_opts)]
 transport_methods = [DGUpwind(eqn, "u"),
-                    DGUpwind(eqn, "rho"),
-                    DGUpwind(eqn, "theta")]
+                     DGUpwind(eqn, "rho"),
+                     DGUpwind(eqn, "theta")]
 
 # Linear Solver
 linear_solver = CompressibleSolver(eqn, alpha=alpha)
 
 # Time Stepper
 stepper = SemiImplicitQuasiNewton(eqn, io, transported_fields,
-                                transport_methods,
-                                linear_solver=linear_solver, alpha=alpha,
-                                num_outer=4, num_inner=1)
+                                  transport_methods,
+                                  linear_solver=linear_solver, alpha=alpha,
+                                  num_outer=4, num_inner=1)
 
 # -------------------------------------------------------------- #
 # Initial Conditions
 # -------------------------------------------------------------- #
-xyz= SpatialCoordinate(mesh)
+xyz = SpatialCoordinate(mesh)
 lon, lat, _ = lonlatr_from_xyz(xyz[0], xyz[1], xyz[2])
 x = xyz[0]
 y = xyz[1]
@@ -108,13 +106,13 @@ g = params.g
 p0 = Constant(100000)
 
 lapse = 0.005
-T0e = 310 # Equatorial temp
-T0p = 240 # Polar surface temp
+T0e = 310  # Equatorial temp
+T0p = 240  # Polar surface temp
 T0 = 0.5 * (T0e + T0p)
-H = Rd * T0 / g # scale height of atmosphere
-k = 3 # power of temp field
-b = 2 # half width parameter
-Vp = 1 # maximum velocity of perturbation
+H = Rd * T0 / g  # scale height of atmosphere
+k = 3  # power of temp field
+b = 2  # half width parameter
+Vp = 1  # maximum velocity of perturbation
 
 u0 = stepper.fields("u")
 rho0 = stepper.fields("rho")
@@ -140,17 +138,17 @@ tao1 = A * lapse / T0 * exp((r - a)*lapse / T0) + B * (1 - 2*((r-a)/(b*H))**2)*e
 tao2 = C * (1 - 2*((r-a)/(b*H))**2)*exp(-((r - a) / (b*H))**2)
 
 tao1_int = A * (exp(lapse * (r - a) / T0) - 1) + B * (r - a) * exp(-((r-a)/(b*H))**2)
-tao2_int = C * (r - a)  * exp(-((r-a) / (b*H))**2)
+tao2_int = C * (r - a) * exp(-((r-a) / (b*H))**2)
 
 # Variable fields
-Temp = (a / r)**2 * (tao1 - tao2 * ( s**k - (k / (k+2)) *s**(k+2)))**(-1)
-P_expr = p0 * exp(-g / Rd * tao1_int + g / Rd * tao2_int * (s**k - (k / (k+2)) *s**(k+2)))
+Temp = (a / r)**2 * (tao1 - tao2 * (s**k - (k / (k + 2)) * s**(k + 2)))**(-1)
+P_expr = p0 * exp(-g / Rd * tao1_int + g / Rd * tao2_int * (s**k - (k / (k+2)) * s**(k+2)))
 
 # wind expression
 wind_proxy = (g / a) * k * Temp * tao2_int * (((r * cos(lat)) / a)**(k-1) - ((r * cos(lat)) / a)**(k+1))
-wind = -omega * r * cos(lat) + sqrt((omega * r * cos(lat))**2 + r * cos(lat) * wind_proxy )
+wind = -omega * r * cos(lat) + sqrt((omega * r * cos(lat))**2 + r * cos(lat) * wind_proxy)
 
-theta_expr = Temp * (P_expr / p0) ** (-params.kappa) 
+theta_expr = Temp * (P_expr / p0) ** (-params.kappa)
 pie_expr = Temp / theta_expr
 rho_expr = P_expr / (Rd * Temp)
 
@@ -158,28 +156,28 @@ rho_expr = P_expr / (Rd * Temp)
 # Perturbation
 # -------------------------------------------------------------- #
 
-base_zonal_u = wind 
-base_merid_u = Constant(0.0) 
+base_zonal_u = wind
+base_merid_u = Constant(0.0)
 
 a = 6.371229e6
 zt = 1.5e4     # top of perturbation
 d0 = a / 6     # horizontal radius of perturbation
-a = 6.371229e6   
+a = 6.371229e6
 err_tol = 1e-12
-lon_c , lat_c = pi/9, 2*pi/9 # location of perturbation centre
+lon_c, lat_c = pi/9, 2*pi/9  # location of perturbation centre
 
-d = a * acos(sin(lat_c)*sin(lat) + cos(lat_c)*cos(lat)*cos(lon - lon_c)) # distance from centre of perturbation
+d = a * acos(sin(lat_c)*sin(lat) + cos(lat_c)*cos(lat)*cos(lon - lon_c))  # distance from centre of perturbation
 
-depth = r - a # The distance from origin subtracted from earth radius
-zeta = conditional(ge(depth,zt-err_tol), 0, 1 - 3*(depth / zt)**2 + 2*(depth / zt)**3) # peturbation vertical taper
+depth = r - a  # The distance from origin subtracted from earth radius
+zeta = conditional(ge(depth, zt-err_tol), 0, 1 - 3*(depth / zt)**2 + 2*(depth / zt)**3)  # peturbation vertical taper
 
-perturb_magnitude = (16*Vp/(3*sqrt(3))) * zeta * sin((pi * d) / (2 * d0)) * cos((pi * d) / (2 * d0)) ** 3
+perturb_magnitude = (16*Vp/(3*sqrt(3))) * zeta * sin((pi * d) / (2 * d0)) * cos((pi * d) / (2 * d0))**3
 
 
-zonal_pert = conditional(le(d,err_tol), 0, 
-                        conditional(ge(d,(d0-err_tol)), 0, -perturb_magnitude * (-sin(lat_c)*cos(lat) + cos(lat_c)*sin(lat)*cos(lon - lon_c)) / sin(d / a)))
-meridional_pert = conditional(le(d,err_tol), 0, 
-                            conditional(ge(d,d0-err_tol), 0, perturb_magnitude * cos(lat_c)*sin(lon - lon_c) / sin(d / a)))
+zonal_pert = conditional(le(d, err_tol), 0,
+                         conditional(ge(d, (d0-err_tol)), 0, -perturb_magnitude * (-sin(lat_c)*cos(lat) + cos(lat_c)*sin(lat)*cos(lon - lon_c)) / sin(d / a)))
+meridional_pert = conditional(le(d, err_tol), 0,
+                              conditional(ge(d, d0-err_tol), 0, perturb_magnitude * cos(lat_c)*sin(lon - lon_c) / sin(d / a)))
 
 zonal_u = base_zonal_u + zonal_pert
 merid_u = base_merid_u + meridional_pert
@@ -194,8 +192,8 @@ print('Set up initial conditions')
 print('project u')
 test_u = TestFunction(Vu)
 dx_reduced = dx(degree=4)
-u_field = zonal_u*e_lon + merid_u * e_lat + radial_u * e_r 
-u_proj_eqn = inner(test_u,u0 - u_field)*dx_reduced
+u_field = zonal_u*e_lon + merid_u * e_lat + radial_u * e_r
+u_proj_eqn = inner(test_u, u0 - u_field) * dx_reduced
 u_proj_prob = NonlinearVariationalProblem(u_proj_eqn, u0)
 u_proj_solver = NonlinearVariationalSolver(u_proj_prob)
 u_proj_solver.solve()
