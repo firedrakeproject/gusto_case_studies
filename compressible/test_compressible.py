357117bdc4cd1e634c1fe410191e26387d8b97f6
Tom Bendall <thomas.bendall@metoffice.gov.uk>
2024-07-25 17:20:41 +0100

    tidy up moist bryan fritsch test case

--- compressible/test_compressible.py
+++ compressible/test_compressible.py
@@ -1,4 +1,5 @@
 from dry_baroclinic_sphere import dry_baroclinic_sphere
+from moist_bryan_fritsch import moist_bryan_fritsch
 
 def test_dry_baroclinic_sphere():
     dry_baroclinic_sphere(
@@ -7,4 +8,13 @@ def test_dry_baroclinic_sphere():
         dt=900,
         tmax=1800,
         dumpfreq=2
+    )
+
+def test_moist_bryan_fritsch():
+    moist_bryan_fritsch(
+        ncolumns=5,
+        nlayers=5,
+        dt=2.0,
+        tmax=10.0,
+        dumpfreq=5
     )
