15a0c05f98c388bc63d360dcde55c1d505b8e1a3
Tom Bendall <thomas.bendall@metoffice.gov.uk>
2024-08-02 09:44:42 +0100

    PR review comments

--- transport/terminator_toy.py
+++ transport/terminator_toy.py
@@ -1,14 +1,13 @@
 from gusto import *
-from firedrake import IcosahedralSphereMesh, Constant, ge, le, exp, cos, \
-    sin, conditional, interpolate, SpatialCoordinate, VectorFunctionSpace, \
-    Function, assemble, dx, FunctionSpace, pi, max_value, acos, as_vector
+from firedrake import IcosahedralSphereMesh, exp, cos, \
+    sin, SpatialCoordinate, pi, max_value, as_vector
 
 import numpy as np
 
 # This script runs the Laurtizen et al. (2015) Terminator Toy
 # test case. This examines the interaction of two species
-# in the transport equation. There is coupling 
-# between the two species to model combination 
+# in the transport equation. There is coupling
+# between the two species to model combination
 # and dissociation.
 
 ######################
@@ -24,7 +23,7 @@ R = 6371220.
 # Domain
 mesh = IcosahedralSphereMesh(radius=R,
                              refinement_level=3, degree=2)
-                             
+
 x = SpatialCoordinate(mesh)
 
 # get lat lon coordinates
@@ -34,17 +33,17 @@ domain = Domain(mesh, dt, 'BDM', 1)
 
 # Define the dry density and the two species as tracers
 rho_d = ActiveTracer(name='rho_d', space='DG',
-                 variable_type=TracerVariableType.density,
-                 transport_eqn=TransportEquationType.conservative)
+                     variable_type=TracerVariableType.density,
+                     transport_eqn=TransportEquationType.conservative)
 
 X = ActiveTracer(name='X', space='DG',
                  variable_type=TracerVariableType.mixing_ratio,
                  transport_eqn=TransportEquationType.advective)
-                 
+
 X2 = ActiveTracer(name='X2', space='DG',
-                 variable_type=TracerVariableType.mixing_ratio,
-                 transport_eqn=TransportEquationType.advective)
-                 
+                  variable_type=TracerVariableType.mixing_ratio,
+                  transport_eqn=TransportEquationType.advective)
+
 tracers = [rho_d, X, X2]
 
 # Equation
@@ -59,9 +58,9 @@ dumpfreq = int(day/dt)
 
 # Set dump_nc = True to use tomplot.
 output = OutputParameters(dirname=dirname,
-                          dumpfreq = dumpfreq,
-                          dump_nc = True,
-                          dump_vtus = True)     
+                          dumpfreq=dumpfreq,
+                          dump_nc=True,
+                          dump_vtus=True)
 
 # Define intermediate sums to be able to use the TracerDensity diagnostic
 X_plus_X2 = Sum('X', 'X2')
@@ -82,6 +81,34 @@ terminator_stepper = BackwardEuler(domain)
 physics_schemes = [(TerminatorToy(eqn, k1=k1, k2=k2, species1_name='X',
                     species2_name='X2'), terminator_stepper)]
 
+# Set up a non-divergent, time-varying, velocity field
+def u_t(t):
+    u_zonal = k*(sin(lamda - 2*pi*t/T)**2)*sin(2*theta)*cos(pi*t/T) + ((2*pi*R)/T)*cos(theta)
+    u_merid = k*sin(2*(lamda - 2*pi*t/T))*cos(theta)*cos(pi*t/T)
+
+    cartesian_u_expr = -u_zonal*sin(lamda) - u_merid*sin(theta)*cos(lamda)
+    cartesian_v_expr = u_zonal*cos(lamda) - u_merid*sin(theta)*sin(lamda)
+    cartesian_w_expr = u_merid*cos(theta)
+
+    u_expr = as_vector((cartesian_u_expr, cartesian_v_expr, cartesian_w_expr))
+
+    return u_expr
+
+# Define limiters for the interacting species
+limiter_space = domain.spaces('DG')
+sublimiters = {'X': DG1Limiter(limiter_space), 'X2': DG1Limiter(limiter_space)}
+MixedLimiter = MixedFSLimiter(eqn, sublimiters)
+
+transport_scheme = SSPRK3(domain, limiter=MixedLimiter)
+transport_method = [DGUpwind(eqn, 'rho_d'), DGUpwind(eqn, 'X'), DGUpwind(eqn, 'X2')]
+
+# Timstepper that solves the physics separately to the dynamics
+# with a defined prescribed transporting velocity
+stepper = SplitPrescribedTransport(eqn, transport_scheme, io,
+                                    spatial_methods=transport_method,
+                                    physics_schemes=physics_schemes,
+                                    prescribed_transporting_velocity=u_t)
+
 # Set up two Gaussian bumps for the initial density field
 theta_c1 = 0.0
 theta_c2 = 0.0
@@ -98,7 +125,7 @@ Z1 = sin(theta_c1)
 
 X2 = cos(theta_c2)*cos(lamda_c2)
 Y2 = cos(theta_c2)*sin(lamda_c2)
-Z2 = sin(theta_c2)   
+Z2 = sin(theta_c2)
 
 g1 = exp(-5*((X-X1)**2 + (Y-Y1)**2 + (Z-Z1)**2))
 g2 = exp(-5*((X-X2)**2 + (Y-Y2)**2 + (Z-Z2)**2))
@@ -115,34 +142,6 @@ X2_0 = 0.5*(X_T_0 - D_val + r)
 
 T = tmax
 k = 10*R/T
-
-# Set up a non-divergent, time-varying, velocity field
-def u_t(t):
-  u_zonal = k*(sin(lamda - 2*pi*t/T)**2)*sin(2*theta)*cos(pi*t/T) + ((2*pi*R)/T)*cos(theta)
-  u_merid = k*sin(2*(lamda - 2*pi*t/T))*cos(theta)*cos(pi*t/T)
-  
-  cartesian_u_expr = -u_zonal*sin(lamda) - u_merid*sin(theta)*cos(lamda)
-  cartesian_v_expr = u_zonal*cos(lamda) - u_merid*sin(theta)*sin(lamda)
-  cartesian_w_expr = u_merid*cos(theta)
-  
-  u_expr = as_vector((cartesian_u_expr, cartesian_v_expr, cartesian_w_expr))
-  
-  return u_expr
-
-# Define limiters for the interacting species
-limiter_space = domain.spaces('DG')
-sublimiters = {'X': DG1Limiter(limiter_space), 'X2': DG1Limiter(limiter_space)}
-MixedLimiter = MixedFSLimiter(eqn, sublimiters)
-
-transport_scheme = SSPRK3(domain, limiter=MixedLimiter)
-transport_method = [DGUpwind(eqn, 'rho_d'), DGUpwind(eqn, 'X'), DGUpwind(eqn, 'X2')]
-
-# Timstepper that solves the physics separately to the dynamics
-# with a defined prescribed transporting velocity
-stepper = SplitPrescribedTransport(eqn, transport_scheme, io, 
-                                    spatial_methods=transport_method,
-                                    physics_schemes=physics_schemes,
-                                    prescribed_transporting_velocity=u_t)
 
 # Initial conditions
 stepper.fields("rho_d").interpolate(rho_expr)
