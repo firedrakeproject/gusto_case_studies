2f1052d9e52cc8f03d54104c0df308bf2c63f6ab
Witt-D <91120222+Witt-D@users.noreply.github.com>
2024-12-17 11:02:48 +0000

    added intial plotting

--- compressible/plot_dry_baroclinic_sphere.py
+++ compressible/plot_dry_baroclinic_sphere.py
@@ -22,31 +22,42 @@ plot_dir = f'{results_dir}/plots'
 plot_stem = f'{plot_dir}/output_testing'
 
 # ---------------------------------------------------------------------------- #
-# Things that should be altered based on the plot
+#Initial plot details
 # ---------------------------------------------------------------------------- #
-# Specify lists for variables that are different between subplots
+
+initial_field_names = ['Temperature', 'u_zonal']
+initial_titles = ['Temperature', 'Zonal wind']
+initial_slices = ['lon', 'lon']
+intial_slice_ats = [0, 0]
+initial_field_labels = [r'$T \ / $K', r'$u \ / $m/s']
+
+## ----------------------------------------------------------------------------#
+# Main plot details
+# ---------------------------------------------------------------------------- #
 field_names = ['Temperature', 'Pressure_Vt']
-titles = ['Temperature', 'Surface Pressure']
-slice_along_values = ['z', 'z']
-
+titles = ['Temperature', 'Pressure']
+slices = ['z', 'z']
+levels = [0, 0]
 field_labels = [r'$T \ / $K', r'$P \ / $Pa']
-
-remove_contour_vals = [None, None]
 domain_limit = {'X': (0, 180), 'Y': (0, 90)}
+xlims = domain_limit['X']
+ylims = domain_limit['Y']
 colour_schemes = ['RdPu', 'RdBu_r']
-contours = ['contour a', 'contour b', 'contour c ', 'contour d']
+temperature_contour = np.arange(220, 320, 10)
+pressure_contour = np.arange(955, 1025, 5 )
+contours = [temperature_contour, pressure_contour]
 
 # Things that are the same for all subplots
-time_idxs = [0, 768, -1]
+time_idxs = [0, 16, -1]
 contour_method = 'tricontour'
-slice_at_vals = [0, 0, 0, 0]
+
 # 1D grids for vertical regridding
 coords_lon_1d = np.linspace(-180, 180, 50)
 coords_lat_1d = np.linspace(-90, 90, 50)
 # Dictionary to hold plotting grids -- keys are "slice_along" values
 plotting_grids = {'lat': coords_lon_1d, 'lon': coords_lat_1d}
 # Level for horizontal slices
-levels = [0, 0]
+
 xlims = domain_limit['X']
 ylims = domain_limit['Y']
 # ---------------------------------------------------------------------------- #
@@ -55,16 +66,63 @@ ylims = domain_limit['Y']
 
 data_file = Dataset(results_file_name, 'r')
 for time_idx in time_idxs:
-    fig, axarray = plt.subplots(2, 1, figsize=(16, 8), sharey='row')
+    if time_idx == 0:
+        fig, axarray = plt.subplots(1, 2, figsize=(8, 16))
+        # Loop through subplots
+        for i, (ax, field_name, field_label, colour_scheme, slice_along, slice_at, title) in \
+            enumerate(zip(axarray.flatten(), initial_field_names, initial_field_labels, colour_schemes,
+                        initial_slices,  initial_slices, initial_titles)):
+            # ------------------------------------------------------------------------ #
+            # Data extraction
+            # ------------------------------------------------------------------------ #
+            orig_field_data, orig_coords_X, orig_coords_Y, orig_coords_Z = \
+                extract_gusto_vertical_slice(data_file, field_name, time_idx,
+                                            slice_along=slice_along, slice_at=slice_at)
 
-    # Loop through subplots
-    for i, (ax, field_name, field_label, colour_scheme, slice_along, remove_contour, slice_at, title, level, contour) in \
-        enumerate(zip(axarray.flatten(), field_names, field_labels, colour_schemes,
-                      slice_along_values, remove_contour_vals, slice_at_vals, titles, levels, contours)):
-        # ------------------------------------------------------------------------ #
-        # Data extraction
-        # ------------------------------------------------------------------------ #
-        if slice_along == 'z':
+            # Slices need regridding as points don't cleanly live along lon or lat = 0.0
+            field_data, coords_hori, coords_Z = regrid_vertical_slice(plotting_grids[slice_along],
+                                                                    slice_along, slice_at,
+                                                                    orig_coords_X, orig_coords_Y,
+                                                                    orig_coords_Z, orig_field_data)
+            time = data_file['time'][time_idx]
+            time_in_days = time / (24*60*60)
+            # ------------------------------------------------------------------------ #
+            # Plot data
+            # ------------------------------------------------------------------------ #
+            contours = tomplot_contours(field_data)
+            if field_name == 'Temperature':
+                contours = np.arange(220, 320, 10)
+
+            cmap, lines = tomplot_cmap(contours, colour_scheme)
+            cf, _ = plot_contoured_field(ax, coords_hori, coords_Z, field_data,
+                                        contour_method, contours, cmap=cmap,
+                                        line_contours=lines)
+            add_colorbar_ax(ax, cf, field_label, location='bottom', cbar_labelpad=-10)
+            # Don't add ylabels unless left-most subplots
+            ylabel = True if i % 3 == 0 else None
+            ylabelpad = -30 if i > 2 else -10
+
+            tomplot_field_title(ax, title, minmax=True, field_data=field_data)
+        fig.subplots_adjust(wspace=0.3, hspace=0.3)
+        fig.suptitle(f'Baroclinic Wave initial fields')
+        # ---------------------------------------------------------------------------- #
+        # Save figure
+        # ---------------------------------------------------------------------------- #
+        plot_name = f'{plot_stem}_intial_fields.png'
+        print(f'Saving figure to {plot_name}')
+        fig.savefig(plot_name, bbox_inches='tight')
+        plt.close()
+
+
+    else:    
+        fig, axarray = plt.subplots(2, 1, figsize=(16, 8), sharey='row')
+        # Loop through subplots
+        for i, (ax, field_name, field_label, colour_scheme, slice_along, title, level, contour) in \
+            enumerate(zip(axarray.flatten(), field_names, field_labels, colour_schemes,
+                          slices, titles, levels, contours)):
+            # ------------------------------------------------------------------------ #
+            # Data extraction
+            # ------------------------------------------------------------------------ #
             # Extraction
             field_full = extract_gusto_field(data_file, field_name, time_idx)
             coords_X_full, coords_Y_full, coords_Z_full = \
@@ -73,55 +131,46 @@ for time_idx in time_idxs:
             # Reshape
             field_full, coords_X_full, coords_Y_full, _ = \
                 reshape_gusto_data(field_full, coords_X_full,
-                                   coords_Y_full, coords_Z_full)
+                                coords_Y_full, coords_Z_full)
 
             # Domain restriction
             field_data, coords_hori, coords_Z = \
                 area_restriction(field_full[:, level], coords_X_full[:, level],
-                                 coords_Y_full[:, level], domain_limit)
-        else:
-            orig_field_data, orig_coords_X, orig_coords_Y, orig_coords_Z = \
-                extract_gusto_vertical_slice(data_file, field_name, time_idx,
-                                             slice_along=slice_along, slice_at=slice_at)
+                                coords_Y_full[:, level], domain_limit)
 
-            # Slices need regridding as points don't cleanly live along lon or lat = 0.0
-            field_data, coords_hori, coords_Z = regrid_vertical_slice(plotting_grids[slice_along],
-                                                                      slice_along, slice_at,
-                                                                      orig_coords_X, orig_coords_Y,
-                                                                      orig_coords_Z, orig_field_data)
-        time = data_file['time'][time_idx]
-        time_in_days = time / (24*60*60)
-        # ------------------------------------------------------------------------ #
-        # Plot data
-        # ------------------------------------------------------------------------ #
-        contours = tomplot_contours(field_data)
-        if field_name == 'Temperature':
-            contours = np.arange(220, 320, 10)
+            time = data_file['time'][time_idx]
+            time_in_days = time / (24*60*60)
+            # ------------------------------------------------------------------------ #
+            # Plot data
+            # ------------------------------------------------------------------------ #
+            contours = tomplot_contours(field_data)
+            if field_name == 'Temperature':
+                contours = np.arange(220, 320, 10)
 
-        cmap, lines = tomplot_cmap(contours, colour_scheme, remove_contour=remove_contour)
-        cf, _ = plot_contoured_field(ax, coords_hori, coords_Z, field_data,
-                                     contour_method, contours, cmap=cmap,
-                                     line_contours=lines)
-        add_colorbar_ax(ax, cf, field_label, location='bottom', cbar_labelpad=-10)
-        # Don't add ylabels unless left-most subplots
-        ylabel = True if i % 3 == 0 else None
-        ylabelpad = -30 if i > 2 else -10
+            cmap, lines = tomplot_cmap(contours, colour_scheme)
+            cf, _ = plot_contoured_field(ax, coords_hori, coords_Z, field_data,
+                                        contour_method, contours, cmap=cmap,
+                                        line_contours=lines)
+            add_colorbar_ax(ax, cf, field_label, location='bottom', cbar_labelpad=-10)
+            # Don't add ylabels unless left-most subplots
+            ylabel = True if i % 3 == 0 else None
+            ylabelpad = -30 if i > 2 else -10
 
-        tomplot_field_title(ax, title, minmax=True, field_data=field_data)
-        ax.set_xlim(xlims)
-        ax.set_xticks(xlims)
-        ax.set_xticklabels(xlims)
-        ax.set_ylim(ylims)
-        ax.set_yticks(ylims)
-        ax.set_yticklabels(ylims)
+            tomplot_field_title(ax, title, minmax=True, field_data=field_data)
+            ax.set_xlim(xlims)
+            ax.set_xticks(xlims)
+            ax.set_xticklabels(xlims)
+            ax.set_ylim(ylims)
+            ax.set_yticks(ylims)
+            ax.set_yticklabels(ylims)
 
-    # These subplots tend to be quite clustered together, so move them apart a bit
-    fig.subplots_adjust(wspace=0.3, hspace=0.3)
-    fig.suptitle(f'Baroclinic Wave at {time_in_days} days')
-    # ---------------------------------------------------------------------------- #
-    # Save figure
-    # ---------------------------------------------------------------------------- #
-    plot_name = f'{plot_stem}_{time_in_days}_days.png'
-    print(f'Saving figure to {plot_name}')
-    fig.savefig(plot_name, bbox_inches='tight')
-    plt.close()
+        # These subplots tend to be quite clustered together, so move them apart a bit
+        fig.subplots_adjust(wspace=0.3, hspace=0.3)
+        fig.suptitle(f'Baroclinic Wave at {time_in_days} days')
+        # ---------------------------------------------------------------------------- #
+        # Save figure
+        # ---------------------------------------------------------------------------- #
+        plot_name = f'{plot_stem}_{time_in_days}_days.png'
+        print(f'Saving figure to {plot_name}')
+        fig.savefig(plot_name, bbox_inches='tight')
+        plt.close()
