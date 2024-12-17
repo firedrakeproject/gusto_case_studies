6568a07303f4ddf973281bf370b57b3d94a23bb6
Witt-D <91120222+Witt-D@users.noreply.github.com>
2024-12-17 11:41:44 +0000

    flake8

--- plotting/compressible_euler/plot_dry_baroclinic.py
+++ plotting/compressible_euler/plot_dry_baroclinic.py
@@ -18,11 +18,11 @@ from tomplot import (tomplot_contours, tomplot_cmap,
 # When copying this example these should not be relative to this file
 results_dir = '/data/home/dw603/firedrake-08_05_24/src/gusto/gusto_case_studies/case_studies/compressible_euler/results/dry_baroclinic_sphere'  # This needs to point to a data file
 results_file_name = f'{results_dir}/field_output.nc'
-plot_dir = f'plot_tests'
+plot_dir = 'plot_tests'  # I used this for testing but unsure how to set up like the other test cases
 plot_stem = f'{plot_dir}/output_testing'
 
 # ---------------------------------------------------------------------------- #
-#Initial plot details
+# Initial plot details
 # ---------------------------------------------------------------------------- #
 
 initial_field_names = ['Temperature', 'u_zonal']
@@ -31,7 +31,7 @@ initial_slices = ['lon', 'lon']
 initial_slice_ats = [0, 0]
 initial_field_labels = [r'$T \ / $K', r'$u \ / $m/s']
 
-## ----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
 # Main plot details
 # ---------------------------------------------------------------------------- #
 field_names = ['Temperature', 'Pressure_Vt']
@@ -44,7 +44,7 @@ xlims = domain_limit['X']
 ylims = domain_limit['Y']
 colour_schemes = ['RdPu', 'RdBu_r']
 temperature_contour = np.arange(220, 320, 10)
-pressure_contour = np.arange(955, 1025, 5 )
+pressure_contour = np.arange(955, 1025, 5)
 contours = [temperature_contour, pressure_contour]
 
 # Things that are the same for all subplots
@@ -71,19 +71,19 @@ for time_idx in time_idxs:
         # Loop through subplots
         for i, (ax, field_name, field_label, colour_scheme, slice_along, slice_at, title) in \
             enumerate(zip(axarray.flatten(), initial_field_names, initial_field_labels, colour_schemes,
-                        initial_slices,  initial_slice_ats, initial_titles)):
+                          initial_slices, initial_slice_ats, initial_titles)):
             # ------------------------------------------------------------------------ #
             # Data extraction
             # ------------------------------------------------------------------------ #
             orig_field_data, orig_coords_X, orig_coords_Y, orig_coords_Z = \
                 extract_gusto_vertical_slice(data_file, field_name, time_idx,
-                                            slice_along=slice_along, slice_at=slice_at)
+                                             slice_along=slice_along, slice_at=slice_at)
 
             # Slices need regridding as points don't cleanly live along lon or lat = 0.0
             field_data, coords_hori, coords_Z = regrid_vertical_slice(plotting_grids[slice_along],
-                                                                    slice_along, slice_at,
-                                                                    orig_coords_X, orig_coords_Y,
-                                                                    orig_coords_Z, orig_field_data)
+                                                                      slice_along, slice_at,
+                                                                      orig_coords_X, orig_coords_Y,
+                                                                      orig_coords_Z, orig_field_data)
             time = data_file['time'][time_idx]
             time_in_days = time / (24*60*60)
             # ------------------------------------------------------------------------ #
@@ -92,8 +92,8 @@ for time_idx in time_idxs:
             auto_contours = tomplot_contours(field_data)
             cmap, lines = tomplot_cmap(auto_contours, colour_scheme)
             cf, _ = plot_contoured_field(ax, coords_hori, coords_Z, field_data,
-                                        contour_method, auto_contours, cmap=cmap,
-                                        line_contours=lines)
+                                         contour_method, auto_contours, cmap=cmap,
+                                         line_contours=lines)
             add_colorbar_ax(ax, cf, field_label, location='bottom', cbar_labelpad=-10)
             # Don't add ylabels unless left-most subplots
             ylabel = True if i % 3 == 0 else None
@@ -101,7 +101,7 @@ for time_idx in time_idxs:
 
             tomplot_field_title(ax, title, minmax=True, field_data=field_data)
         fig.subplots_adjust(wspace=0.3, hspace=0.3)
-        fig.suptitle(f'Baroclinic Wave initial fields')
+        fig.suptitle('Baroclinic Wave initial fields')
         # ---------------------------------------------------------------------------- #
         # Save figure
         # ---------------------------------------------------------------------------- #
@@ -110,8 +110,7 @@ for time_idx in time_idxs:
         fig.savefig(plot_name, bbox_inches='tight')
         plt.close()
 
-
-    else:    
+    else:
         fig, axarray = plt.subplots(2, 1, figsize=(16, 8), sharey='row')
         # Loop through subplots
         for i, (ax, field_name, field_label, colour_scheme, slice_along, title, level, contour) in \
@@ -128,12 +127,12 @@ for time_idx in time_idxs:
             # Reshape
             field_full, coords_X_full, coords_Y_full, _ = \
                 reshape_gusto_data(field_full, coords_X_full,
-                                coords_Y_full, coords_Z_full)
+                                   coords_Y_full, coords_Z_full)
 
             # Domain restriction
             field_data, coords_hori, coords_Z = \
                 area_restriction(field_full[:, level], coords_X_full[:, level],
-                                coords_Y_full[:, level], domain_limit)
+                                 coords_Y_full[:, level], domain_limit)
 
             time = data_file['time'][time_idx]
             time_in_days = time / (24*60*60)
@@ -143,8 +142,8 @@ for time_idx in time_idxs:
 
             cmap, lines = tomplot_cmap(contour, colour_scheme)
             cf, _ = plot_contoured_field(ax, coords_hori, coords_Z, field_data,
-                                        contour_method, contour, cmap=cmap,
-                                        line_contours=lines)
+                                         contour_method, contour, cmap=cmap,
+                                         line_contours=lines)
             add_colorbar_ax(ax, cf, field_label, location='bottom', cbar_labelpad=-10)
             # Don't add ylabels unless left-most subplots
             ylabel = True if i % 3 == 0 else None
