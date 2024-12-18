204b3f24eda9d42e48b51af32a9ce75e73cfa586
Thomas Bendall <thomas.bendall@metoffice.gov.uk>
2024-12-18 15:40:23 +0000

    Fix wind used in four-part SBR test (#33)

--- plotting/transport/plot_four_part_sbr.py
+++ plotting/transport/plot_four_part_sbr.py
@@ -25,7 +25,7 @@ test = 'four_part_sbr'
 # When copying this example these paths need editing, which will usually involve
 # removing the abspath part to set directory paths relative to this file
 results_file_name = f'{abspath(dirname(__file__))}/../../results/{test}/field_output.nc'
-plot_stem = f'{abspath(dirname(__file__))}/../figures/{test}'
+plot_stem = f'{abspath(dirname(__file__))}/../../figures/transport/{test}'
 
 # ---------------------------------------------------------------------------- #
 # Plot details
